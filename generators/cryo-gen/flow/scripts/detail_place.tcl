7094179a30381c027888140d4d6c75bf9139bd16
tuohangzeng <zengt2@rpi.edu>
2022-01-02 21:05:16 +0000

    Added cryo support for sky130_OSU_18T_hs platform
    Signed-off-by: tuohangzeng <zengt2@rpi.edu>

--- generators/cryo-gen/flow/scripts/detail_place.tcl
+++ generators/cryo-gen/flow/scripts/detail_place.tcl
@@ -41,6 +41,10 @@ set_placement_padding -left 0 -right 0 -masters sky130_osu_sc_12T_hs__inv_1
 set_placement_padding -left 1 -right 1 -masters sky130_osu_sc_12T_hs__decap_1
 set_placement_padding -left 0 -right 0 -masters sky130_osu_sc_12T_hs__nand2_1
 
+set_placement_padding -left 0 -right 0 -masters sky130_osu_sc_18T_hs__inv_1
+set_placement_padding -left 1 -right 1 -masters sky130_osu_sc_18T_hs__decap_1
+set_placement_padding -left 0 -right 0 -masters sky130_osu_sc_18T_hs__nand2_1
+
 
 #set db [ord::get_db]
 #set tech [$db getTech]
