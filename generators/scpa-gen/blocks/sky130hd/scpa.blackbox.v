7c619eb1476e11d417d3e0b07b7714e661a1ed52
Li-Yu Chen <chenliyu@clinton.eecs.umich.edu>
2022-02-28 16:12:07 -0500

    scpa first commit

---
+++ generators/scpa-gen/blocks/sky130hd/scpa.blackbox.v
@@ -0,0 +1,6 @@
+(* blackbox *) module mimcaptut(
+  inout top,
+  inout bot
+);
+parameter dont_touch = "on";
+endmodule
---
+++ generators/scpa-gen/flow/scripts/global_place.tcl
@@ -0,0 +1,104 @@
+if {![info exists standalone] || $standalone} {
+  # Read lef
+  read_lef $::env(TECH_LEF)
+  read_lef $::env(SC_LEF)
+  if {[info exist ::env(ADDITIONAL_LEFS)]} {
+    foreach lef $::env(ADDITIONAL_LEFS) {
+      read_lef $lef
+    }
+  }
+
+  # Read liberty files
+  foreach libFile $::env(LIB_FILES) {
+    read_liberty $libFile
+  }
+
+  # Read design files
+  read_def $::env(RESULTS_DIR)/2_floorplan.def
+  read_sdc $::env(RESULTS_DIR)/2_floorplan.sdc
+  if [file exists $::env(PLATFORM_DIR)/derate.tcl] {
+    source $::env(PLATFORM_DIR)/derate.tcl
+  }
+} else {
+  puts "Starting global placement"
+}
+
+
+# Set res and cap
+source $::env(PLATFORM_DIR)/setRC.tcl
+set_dont_use $::env(DONT_USE_CELLS)
+
+if { 0 != [llength [array get ::env GLOBAL_PLACEMENT_ARGS]] } {
+global_placement -routability_driven -density $::env(PLACE_DENSITY) \
+    -pad_left $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
+    -pad_right $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
+    $::env(GLOBAL_PLACEMENT_ARGS)
+} else {
+global_placement -routability_driven -density $::env(PLACE_DENSITY) \
+    -pad_left $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
+    -pad_right $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT)
+}
+
+set db [ord::get_db]
+set tech [$db getTech]
+set libs [$db getLibs]
+set block [[$db getChip] getBlock]
+
+#set region [$block findRegion "mimcaptut_array"]
+#set rect [lindex [$region getBoundaries] 0]
+
+#set domain_xMin [$rect xMin]
+#set domain_yMin [$rect yMin]
+#set domain_xMax [$rect xMax]
+#set domain_yMax [$rect yMax]
+
+foreach inst [$block getInsts] {
+  if {[[$inst getMaster] getName] == "mimcaptut"} {
+    $inst setOrigin 6 6
+  }
+}
+
+
+#set db [ord::get_db]
+#set block [[$db getChip] getBlock]
+#set tech [$db getTech]
+#
+#set core [$block getCoreArea]
+#set core_xl [$core xMin]
+#set core_yl [$core yMin]
+#set core_xh [$core xMax]
+#set core_yh [$core yMax]
+#
+#set div_cen_x [expr double(($core_xl + $core_xh) * 3 / 4 / 1000)]
+#set div_cen_y [expr double(($core_yl + $core_yh) / 2 / 1000)]
+#
+#set div_cen [concat $div_cen_x $div_cen_y]
+#
+#set allInst [$block getInsts]
+
+#foreach inst $allinsts {
+#  set master [$inst getMaster]
+#  set name [$inst getName]
+#  if {[string match "mimcaptut" $name]} {
+#        place_cell -inst $name \
+#        -origin $div_cen \
+#        -orient R0
+#  }
+#}
+
+#foreach inst [$block getInsts] {
+#  if {[[$inst getMaster] getName] == "mimcaptut"} {
+#    $inst setOrigin $div_cen
+#  }
+#}
+
+estimate_parasitics -placement
+report_wns
+report_tns
+report_worst_slack
+report_design_area
+
+if {![info exists standalone] || $standalone} {
+  write_def $::env(RESULTS_DIR)/3_1_place_gp.def
+  exit
+}
