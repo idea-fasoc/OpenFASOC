c38bfd2a00044c055a1ad6075c0003fe8048365d
Pranav Lulu <61928883+lulu9312@users.noreply.github.com>
2022-08-25 09:05:25 +0530

    Config file updated
    Changes made in config file as per ORFS

--- openfasoc/common/platforms/sky130hvl/config.mk
+++ openfasoc/common/platforms/sky130hvl/config.mk
@@ -2,7 +2,7 @@
 export PROCESS = 130
 
 # Rules for metal fill
-export FILL_CONFIG = ../../../common/platforms/$(PLATFORM)/fill.json
+export FILL_CONFIG = ./platforms/$(PLATFORM)/fill.json
 
 # Set the TIEHI/TIELO cells
 # These are used in yosys synthesis to avoid logical 1/0's in the netlist
@@ -17,39 +17,58 @@ export MAX_FANOUT = 5
 
 # Blackbox verilog file
 # List all standard cells and cells yosys should treat as blackboxes here
-export BLACKBOX_V_FILE = ../../../common/platforms/$(PLATFORM)/sky130_fd_sc_hvl.blackbox.v
+export BLACKBOX_V_FILE = ./platforms/$(PLATFORM)/sky130_fd_sc_hvl.blackbox.v
 
 # Yosys mapping files
 #export LATCH_MAP_FILE = ../../../common/platforms/$(PLATFORM)/cells_latch_hvl.v
 #export CLKGATE_MAP_FILE = ../../../common/platforms/$(PLATFORM)/cells_clkgate_hvl.v
 #export BLACKBOX_MAP_TCL = ../../../common/platforms/$(PLATFORM)/blackbox_map.tcl
+#Floorplan initialiazation
+
+export DIE_AREA   	 	= 0 0 300 350
+export CORE_AREA   		= 15 15 285 335
+
 
 # Placement site for core cells
 # This can be found in the technology lef
 export PLACE_SITE = unithv
 
-export MACRO_PLACE_HALO ?= 1 1
-export MACRO_PLACE_CHANNEL ?= 80 80
+export PLACE_DENSITY = 0.30
+#export PLACE_MAX_PHI_COEF = 1.00
 
-export TECH_LEF = ../../../common/platforms/$(PLATFORM)/lef/sky130_fd_sc_hvl.tlef
-export SC_LEF = ../../../common/platforms/$(PLATFORM)/lef/sky130_fd_sc_hvl_merged.lef
 
-export LIB_FILES = ../../../common/platforms/$(PLATFORM)/lib/sky130_fd_sc_hvl__tt_025C_3v30.lib \
+export TECH_LEF = ./platforms/$(PLATFORM)/lef/sky130_fd_sc_hvl.tlef
+
+export SC_LEF = ./platforms/$(PLATFORM)/lef/sky130_fd_sc_hvl_merged.lef
+
+
+export LIB_FILES = ./platforms/$(PLATFORM)/lib/sky130_fd_sc_hvl__tt_025C_3v30.lib \
                      $(ADDITIONAL_LIBS)
-export GDS_FILES = $(wildcard ../../../common/platforms/$(PLATFORM)/gds/*.gds) \
-                     $(ADDITIONAL_GDS_FILES)
+
+export GDS_FILES = ./platforms/$(PLATFORM)/gds/sky130_fd_sc_hvl.gds \
+	             $(ADDITIONAL_GDS)
+
+
+
+#export DOMAIN_INSTS_LIST 	= ./platforms/$(PLATFORM)/ldo_domain_insts.txt
+
 
 # Cell padding in SITE widths to ease rout-ability
 export CELL_PAD_IN_SITES = 4
 
 # Endcap and Welltie cells
-export TAPCELL_TCL = ../../../common/platforms/$(PLATFORM)/tapcell.tcl
+export TAPCELL_TCL = ./platforms/$(PLATFORM)/tapcell.tcl
 
 # TritonCTS options
 export CTS_BUF_CELL   = sky130_fd_sc_hvl__buf_1
 export CTS_MAX_SLEW   = 1.5e-9
 export CTS_MAX_CAP    = .1532e-12
-export CTS_TECH_DIR   = ../../../common/platforms/$(PLATFORM)/tritonCTShd
+export CTS_TECH_DIR   = ./platforms/$(PLATFORM)/tritonCTShd
+
+export MACRO_PLACE_HALO ?= 1 1
+export MACRO_PLACE_CHANNEL ?= 20 20
+#export MACRO_FENCE_REGION = 250 30 490 170
+export MACRO_PLACEMENT = ./platforms/$(PLATFORM)/macro.tcl
 
 # FastRoute options
 export MIN_ROUTING_LAYER = met1
@@ -62,7 +81,7 @@ export IO_PIN_MARGIN = 70
 export WIRE_RC_LAYER = met3
 
 # KLayout technology file
-export KLAYOUT_TECH_FILE = ../../../common/platforms/$(PLATFORM)/$(PLATFORM).lyt
+export KLAYOUT_TECH_FILE = ./platforms/$(PLATFORM)/$(PLATFORM).lyt
 
 # Dont use cells to ease congestion
 # Specify at least one filler cell if none
@@ -78,16 +97,17 @@ export ABC_DRIVER_CELL = sky130_fd_sc_hvl__buf_1
 export ABC_LOAD_IN_FF = 5
 #export ABC_CLOCK_PERIOD_IN_PS = 10
 
-# Define default PDN config
-export PDN_CFG ?= ../../../common/platforms/$(PLATFORM)/pdn.cfg
+
+#Define default PDN config
+export PDN_TCL ?= ./platforms/$(PLATFORM)/pdn.tcl
 
 # Define fastRoute tcl
-export FASTROUTE_TCL = ../../../common/platforms/$(PLATFORM)/fastroute.tcl
+export FASTROUTE_TCL = ./platforms/$(PLATFORM)/fastroute.tcl
 
 # Template definition for power grid analysis
-export TEMPLATE_PGA_CFG ?= ../../../common/platforms/sky130/template_pga.cfg
+export TEMPLATE_PGA_CFG ?= ./platforms/sky130/template_pga.cfg
 
-export PLACE_DENSITY ?= 0.60
+
 
 # Define Hold Buffer
 export HOLD_BUF_CELL = sky130_fd_sc_hvl__buf_1
@@ -97,8 +117,8 @@ export IO_PLACER_H = met3
 export IO_PLACER_V = met2
 
 # keep with gf
-export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 4
-export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 2
+export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 1
+export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 0
 
 # Define fill cells
 export FILL_CELLS = sky130_fd_sc_hvl__fill_1 sky130_fd_sc_hvl__fill_2 sky130_fd_sc_hvl__fill_4 sky130_fd_sc_hvl__fill_8
@@ -106,4 +126,12 @@ export FILL_CELLS = sky130_fd_sc_hvl__fill_1 sky130_fd_sc_hvl__fill_2 sky130_fd_
 # resizer repair_long_wires -max_length
 export MAX_WIRE_LENGTH = 21000
 
-export CDL_FILE = ../../../common/platforms/sky130hvl/cdl/sky130_fd_sc_hvl.spice
+export CDL_FILE = ./platforms/sky130hvl/cdl/sky130_fd_sc_hvl.spice
+
+#export ENABLE_DPO = 1
+#export DPO_MAX_DISPLACEMENT = 390 390
+# IR drop estimation supply net name to be analyzed and supply voltage variable
+# For multiple nets: PWR_NETS_VOLTAGES  = "VDD1 1.8 VDD2 1.2"
+#export PWR_NETS_VOLTAGES  ?= "VDD 3.3"
+#export GND_NETS_VOLTAGES  ?= "VSS 0.0"
+#export IR_DROP_LAYER = met1
