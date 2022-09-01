ee5eff26213efbfaa147c75c61f824fba167ae94
saicharan0112 <saicharan00112@gmail.com>
2022-09-01 18:20:50 +0530

    Update requirements file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
+import sphinx_rtd_theme, nbsphinx
 
 html_theme = "sphinx_rtd_theme"
 
