3204ee3b1d793231fcf7484277572e6f6ad5bfff
Pranav <pranavlulu87@gmail.com>
2022-12-30 18:11:45 +0530

    Removed commented code and fixed shell variable error

--- openfasoc/common/drc-lvs-check/run_lvspex.sh
+++ openfasoc/common/drc-lvs-check/run_lvspex.sh
@@ -3,33 +3,25 @@
 cd $OBJECTS_DIR/netgen_lvs/ext/
 
 # generate lvs netlist using magic
-magic -rcfile $COMMON_VERIF_DIR/sky130A/sky130A.magicrc -noconsole -dnull << EOF
+cat > magic.script <<EOF
+gds flatglob *\$\$*
 gds read $1
 load $2
-# gds flatglob $2
-# flatten $2_flat
-# load $2_flat
-# gds write ./FLATTEN_GDS/$2_flat.gds
-# EOF
-# magic -rcfile ./sky130A/sky130A.magicrc -noconsole -dnull << EOF
-# gds read ./FLATTEN_GDS/$2_flat.gds
-# load $2_flat
-# flatten $2
-# load $2
-# gds write ./FLATTEN_GDS/$2.gds
-# EOF
-# magic -rcfile ./sky130A/sky130A.magicrc -noconsole -dnull << EOF
-# gds read ./FLATTEN_GDS/$2.gds
-# load $2
+select top cell
+flatten flat_design
+load flat_design
+select top cell
 extract all
 ext2spice lvs
-# ext2spice merge aggressive
 ext2spice -o ../spice/$2_lvsmag.spice
-extract all
+gds read $1
+load $2  
+extract all 
 ext2spice lvs
 ext2spice rthresh 0
 ext2spice cthresh 0
 ext2spice -o ../spice/$2_pex.spice
+gds read $1
 load $2
 extract all
 ext2spice cthresh 0
@@ -37,14 +29,16 @@ ext2spice -o ../spice/$2_sim.spice
 exit
 EOF
 
+magic -rcfile $COMMON_VERIF_DIR/sky130A/sky130A.magicrc -noconsole -dnull < magic.script
+
 # Adapt the extracted spice file to account for errors in Magic
 # Importantly, this script is specific in what it looks for,
 # so is unlikely to break LVS if Magic improves in the future
 # note that --toplevel is optional (specify if you have a top level subckt)
 # note also that you must have the global variable "__open_generator_name__" specified to your generator (as described in the python script)
-python3 $COMMON_VERIF_DIR/process_extracted_pins.py --lvsmag $OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice --toplevel $2 --generator $__open_generator_name__
+python3 $COMMON_VERIF_DIR/process_extracted_pins.py --lvsmag $OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice --toplevel flat_design --generator $__open_generator_name__
 
 # run lvs check using netgen
 # netgen lvs $2_lvsmag.spice $2.spice $COMMON_VERIF_DIR/sky130A/sky130A_setup.tcl $3 -full
 # Run netgen in batch mode
-netgen -batch lvs "$OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice $2" "$OBJECTS_DIR/netgen_lvs/spice/$2.spice $2" $COMMON_VERIF_DIR/sky130A/sky130A_setup.tcl $3
+netgen -batch lvs "$OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice flat_design" "$OBJECTS_DIR/netgen_lvs/spice/$2.spice $2" $COMMON_VERIF_DIR/sky130A/sky130A_setup.tcl $3
