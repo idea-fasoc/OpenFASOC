9cf01f549ebeaf82e807ccc5ad4cded012254c97
saicharan0112 <saicharan00112@gmail.com>
2023-01-01 11:36:55 +0530

    don't just print the message. Raise the flag and exit the flow when the drc and lvs fails

--- openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
+++ openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
@@ -2,7 +2,7 @@ drc_filename = "flow/reports/sky130hd/tempsense/6_final_drc.rpt"
 num_lines = sum(1 for line in open(drc_filename))
 
 if num_lines > 3:
-    print("DRC failed!")
+    raise ValueError("DRC failed!")
 else:
     print("DRC is clean!")
 
@@ -13,6 +13,6 @@ with open(lvs_filename) as f:
     f1 = f.read()
 
     if "failed" in f1:
-        print("LVS failed!")
+        raise ValueError("LVS failed!")
     else:
         print("LVS is clean!")
