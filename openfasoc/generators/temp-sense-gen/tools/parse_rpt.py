259daeb995e52dc44a0757291bb796d39d1a8b9d
saicharan0112 <saicharan00112@gmail.com>
2023-01-01 11:10:32 +0530

    modify lvs parser to catch failed error msgs - fixes #114

--- openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
+++ openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
@@ -1,4 +1,3 @@
-import subprocess, sys, re
 
 drc_filename = "flow/reports/sky130hd/tempsense/6_final_drc.rpt"
 num_lines = sum(1 for line in open(drc_filename))
@@ -9,17 +8,13 @@ else:
     print("DRC is clean!")
 
 
-# LVS Bypassed
-
 lvs_filename = "flow/reports/sky130hd/tempsense/6_final_lvs.rpt"
-lvs_line = subprocess.check_output(["tail", "-1", lvs_filename]).decode(
-    sys.stdout.encoding
-)
 
-regex = r"Netlists do not match"
-match = re.search(regex, lvs_line)
+with open(lvs_filename) as f:
+    f1=f.read()
 
-if match != None:
-    raise ValueError("LVS failed!")
-else:
-    print("LVS is clean!")
+    if "failed" in f1:
+        print("LVS failed!")
+    else:
+        print("LVS is clean!")
+
