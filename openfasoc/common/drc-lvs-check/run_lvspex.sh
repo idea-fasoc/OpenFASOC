d6d79344c4427c671182a7b4686c37509b461767
Pranav <pranavlulu87@gmail.com>
2023-01-02 12:21:41 +0530

    Updated the file according to ldo-gen.This should not break other generators

--- openfasoc/common/drc-lvs-check/run_lvspex.sh
+++ openfasoc/common/drc-lvs-check/run_lvspex.sh
@@ -7,21 +7,26 @@ cat > magic.script <<EOF
 gds flatglob *\$\$*
 gds read $1
 load $2
-select top cell
-flatten flat_design
-load flat_design
-select top cell
+
+if {![string compare $2 "ldoInst"]} {
+  select top cell
+  flatten ldoInst_flat
+  load ldoInst_flat
+  cellname delete ldoInst
+  cellname rename ldoInst_flat ldoInst
+  select top cell
+} else {
+    select top cell
+}
 extract all
 ext2spice lvs
 ext2spice -o ../spice/$2_lvsmag.spice
-gds read $1
 load $2
 extract all
 ext2spice lvs
 ext2spice rthresh 0
 ext2spice cthresh 0
 ext2spice -o ../spice/$2_pex.spice
-gds read $1
 load $2
 extract all
 ext2spice cthresh 0
@@ -36,9 +41,9 @@ magic -rcfile $COMMON_VERIF_DIR/sky130A/sky130A.magicrc -noconsole -dnull < magi
 # so is unlikely to break LVS if Magic improves in the future
 # note that --toplevel is optional (specify if you have a top level subckt)
 # note also that you must have the global variable "__open_generator_name__" specified to your generator (as described in the python script)
-python3 $COMMON_VERIF_DIR/process_extracted_pins.py --lvsmag $OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice --toplevel flat_design --generator $__open_generator_name__
+python3 $COMMON_VERIF_DIR/process_extracted_pins.py --lvsmag $OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice --toplevel $2 --generator $__open_generator_name__
 
 # run lvs check using netgen
 # netgen lvs $2_lvsmag.spice $2.spice $COMMON_VERIF_DIR/sky130A/sky130A_setup.tcl $3 -full
 # Run netgen in batch mode
-netgen -batch lvs "$OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice flat_design" "$OBJECTS_DIR/netgen_lvs/spice/$2.spice $2" $COMMON_VERIF_DIR/sky130A/sky130A_setup.tcl $3
+netgen -batch lvs "$OBJECTS_DIR/netgen_lvs/spice/$2_lvsmag.spice $2" "$OBJECTS_DIR/netgen_lvs/spice/$2.spice $2" $COMMON_VERIF_DIR/sky130A/sky130A_setup.tcl $3
