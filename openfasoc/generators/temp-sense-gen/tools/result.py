b912cd66106be5e41cf31e9b3cdbcfefe4782cbe
saicharan0112 <saicharan00112@gmail.com>
2023-02-04 13:17:02 +0000

    modifications for xyce sim too

--- openfasoc/generators/temp-sense-gen/tools/result.py
+++ openfasoc/generators/temp-sense-gen/tools/result.py
@@ -19,18 +19,32 @@ with open(file_name, "r") as rf:
     temp_value = "failed"
     if temp_value_re:
         temp_value = temp_value_re.group(1)
+    else:
+        temp_value = file_name.split("_")[-1].split(".")[0]
 
     period_pattern = "PERIOD\s*=\s*([0-9\.e-]+)"
     period_pattern_re = re.search(period_pattern, log_file_text)
     period_value = "failed"
     if period_pattern_re:
         period_value = period_pattern_re.group(1)
+    else:
+        period_pattern = "period\s*=\s*([0-9\.e-]+)"
+        period_pattern_re = re.search(period_pattern, log_file_text)
+        period_value = "failed"
+        if period_pattern_re:
+            period_value = period_pattern_re.group(1)
 
     power_pattern = "POWER\s*=\s*([0-9\.e-]+)"
     power_pattern_re = re.search(power_pattern, log_file_text)
     power_value = "failed"
     if power_pattern_re:
         power_value = power_pattern_re.group(1)
+    else:
+        power_pattern = "power\s*=\s*([0-9\.e-]+)"
+        power_pattern_re = re.search(power_pattern, log_file_text)
+        power_value = "failed"
+        if power_pattern_re:
+            power_value = power_pattern_re.group(1)
 
     print(
         "%s	%s %s" % (temp_value, period_value, power_value),
