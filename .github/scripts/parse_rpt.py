7d360ab9fbab51105e081e30cc7fac1b993a49e7
Sai Charan <saicharan00112@gmail.com>
2023-04-14 21:59:44 +0530

    modify parse_rpt

--- .github/scripts/parse_rpt.py
+++ .github/scripts/parse_rpt.py
@@ -1,6 +1,4 @@
-import sys
-generator = sys.argv[1]
-drc_filename = "openfasoc/generators/"+generator+"/work/6_final_drc.rpt"
+drc_filename = "work/6_final_drc.rpt"
 num_lines = sum(1 for line in open(drc_filename))
 
 if num_lines > 3:
@@ -9,7 +7,7 @@ else:
     print("DRC is clean!")
 
 
-lvs_filename = "openfasoc/generators/"+generator+"work/6_final_lvs.rpt"
+lvs_filename = "work/6_final_lvs.rpt"
 
 with open(lvs_filename) as f:
     f1 = f.read()
