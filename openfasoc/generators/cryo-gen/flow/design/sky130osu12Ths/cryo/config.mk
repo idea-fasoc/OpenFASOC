ec700c46370eb1e280b3dadb4906562e464d44ae
Mehdi Saligane <48068702+msaligane@users.noreply.github.com>
2023-05-16 23:39:12 -0400

    Update config.mk

--- openfasoc/generators/cryo-gen/flow/design/sky130osu12Ths/cryo/config.mk
+++ openfasoc/generators/cryo-gen/flow/design/sky130osu12Ths/cryo/config.mk
@@ -11,21 +11,7 @@ export SDC_FILE    		= ./design/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 export DIE_AREA   	 	= 0 0 100 40
 export CORE_AREA   		= 4 4 96 36
 
-#export PDN_CFG 			= ../blocks/$(PLATFORM)/pdn.cfg
 export PDN_TCL 			= ../blocks/$(PLATFORM)/pdn.tcl
-# export ADDITIONAL_LEFS  	= ../blocks/$(PLATFORM)/lef/HEADER.lef \
-#                         	  ../blocks/$(PLATFORM)/lef/SLC.lef
-
-# export ADDITIONAL_GDS_FILES 	= ../blocks/$(PLATFORM)/gds/HEADER.gds \
-# 			      	  ../blocks/$(PLATFORM)/gds/SLC.gds
-
-# export DOMAIN_INSTS_LIST 	= ../blocks/$(PLATFORM)/$(DESIGN_NAME)_domain_insts.txt
-
-# export CUSTOM_CONNECTION 	= ../blocks/$(PLATFORM)/$(DESIGN_NAME)_custom_net.txt
-
-#export ADD_NDR_RULE		= 1
-#export NDR_RULE_NETS 		= r_VIN
-#export NDR_RULE 		= NDR_2W_2S
 
 
 # RO inverter placement config
