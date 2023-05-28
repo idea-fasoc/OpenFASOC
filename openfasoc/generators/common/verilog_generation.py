7c58dbd5791e3add18dd9a6eec4c217c4e79fe6e
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-05-29 00:51:21 +0530

    doc: added code hints to verilog generation functions

--- openfasoc/generators/common/verilog_generation.py
+++ openfasoc/generators/common/verilog_generation.py
@@ -1,16 +1,16 @@
 from mako.template import Template
 from os import path, makedirs, listdir
 
-def __get_output_filepath(filename, out_dir):
+def __get_output_filepath(filename: str, out_dir: str) -> str:
 	return path.join(out_dir, filename.replace(".template", ""))
 
-def __generate_file(input_path, output_path, parameters):
+def __generate_file(input_path: str, output_path: str, parameters: dict) -> None:
 	template = Template(filename=input_path)
 
 	out_file = open(output_path, "w")
 	out_file.write(template.render(**parameters))
 
-def __generate_subdirectory(src_dir, out_dir, parameters):
+def __generate_subdirectory(src_dir: str, out_dir: str, parameters: dict) -> None:
 	# generate the output directory if it doesn't exist
 	if not path.exists(out_dir):
 		makedirs(out_dir)
@@ -30,8 +30,8 @@ def __generate_subdirectory(src_dir, out_dir, parameters):
 			)
 
 def generate_verilog(
-	parameters,
-	src_dir = "src",
-	out_dir = path.join("flow", "design", "src")
-):
+	parameters: dict,
+	src_dir: str = "src",
+	out_dir: str = path.join("flow", "design", "src")
+) -> None:
 	__generate_subdirectory(src_dir, out_dir, parameters)
