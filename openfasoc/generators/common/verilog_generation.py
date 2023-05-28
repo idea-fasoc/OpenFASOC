45d2c600a615613c7407fa9746263e5421ee5b04
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-05-28 23:03:53 +0530

    feat: exported a verilog generation function

---
+++ openfasoc/generators/common/verilog_generation.py
@@ -0,0 +1,37 @@
+from mako.template import Template
+from os import path, makedirs, listdir
+
+def __get_output_filepath(filename, out_dir):
+	return path.join(out_dir, filename.replace(".template", ""))
+
+def __generate_file(input_path, output_path, parameters):
+	template = Template(filename=input_path)
+
+	out_file = open(output_path, "w")
+	out_file.write(template.render(**parameters))
+
+def __generate_subdirectory(src_dir, out_dir, parameters):
+	# generate the output directory if it doesn't exist
+	if not path.exists(out_dir):
+		makedirs(out_dir)
+
+	for filename in listdir(src_dir):
+		file_path = path.join(src_dir, filename)
+
+		if path.isdir(file_path):
+			# if the path is a subdirectory, recursively call the function
+			__generate_subdirectory(file_path, path.join(out_dir, filename), parameters)
+		else:
+			# if the path is a fine, generate the output
+			__generate_file(
+				file_path,
+				__get_output_filepath(filename, out_dir),
+				parameters
+			)
+
+def generate_verilog(
+	parameters,
+	src_dir = "src",
+	out_dir = path.join("flow", "design", "src")
+):
+	__generate_subdirectory(src_dir, out_dir, parameters)
