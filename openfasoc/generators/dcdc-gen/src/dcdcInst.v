de601d541c9acc9b32061df2df67fa207e6d64a2
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-08 20:33:52 +0530

    fix<dcdc>: disabled the noise injection module * LFSR module used in the noise injection module was missing

--- openfasoc/generators/dcdc-gen/src/dcdcInst.v
+++ openfasoc/generators/dcdc-gen/src/dcdcInst.v
@@ -9,10 +9,10 @@ module ${design_name} (
     input clk,
 	input VREF_in, //new added input
 	input [5:0] sel_vh, sel_vl,
-	input [1:0] s,
-	input non_clk_rst_n, //give a low-level voltage to reset seeds at the beginnnig
-	input non_clk_control, //inject random noise all the time
-	input [8:0] non_clk_seed //inject seeds of random noise at reset time
+	input [1:0] s
+	// input non_clk_rst_n //give a low-level voltage to reset seeds at the beginnnig
+	// input non_clk_control, //inject random noise all the time
+	// input [8:0] non_clk_seed //inject seeds of random noise at reset time
 );
 
     wire w_clk0, w_clk0b, w_clk1, w_clk1b;
@@ -39,13 +39,13 @@ module ${design_name} (
     );
 
 	//DCDE_DIGITAL_NOISE_INJECTION
-	DCDC_DIGITAL_NOISE_INJECTION u_DCDC_DIGITAL_NOISE_INJECTION(
-		.CLK(clk),
-		.rst_n(non_clk_rst_n),
-		.control(non_clk_control),
-		.seed(non_clk_seed),
-		.Q(non_clk_dcde_noise_injection)
-	);
+	// DCDC_DIGITAL_NOISE_INJECTION u_DCDC_DIGITAL_NOISE_INJECTION(
+	// 	.CLK(clk),
+	// 	.rst_n(non_clk_rst_n),
+	// 	.control(non_clk_control),
+	// 	.seed(non_clk_seed),
+	// 	.Q(non_clk_dcde_noise_injection)
+	// );
 
 	// AUX CELL DCDC_COMP
 	DCDC_COMP u_DCDC_COMP(
