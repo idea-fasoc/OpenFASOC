1aae2881de31c154ad7571e7c60f547ee311ef55
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-08-06 18:41:44 +0530

    feat: print the average time per simulation

--- openfasoc/generators/common/simulation/utils.py
+++ openfasoc/generators/common/simulation/utils.py
@@ -5,12 +5,12 @@ def _print_progress(total_runs: int, run_number: int, start_time: int, end: str 
 
 	Displays the number of simulations completed, total simulations and the time elapsed.
 	"""
-	print(f"Completed {run_number} out of {total_runs} simulations. Elapsed time: {_format_elapsed_time(start_time)}", end=end)
+	print(f"Completed {run_number} out of {total_runs} simulations. Elapsed time: {_format_elapsed_time(start_time)}{f'. Average: {_format_elapsed_time(start_time, run_number)} per simulation.' if run_number > 0 else ''}", end=end)
 
-def _format_elapsed_time(start_time: int):
+def _format_elapsed_time(start_time: int, divisor: int = 1):
 	"""Formats the elapsed time (in seconds) into hours, minutes, and seconds format.
 	"""
-	elapsed_seconds = int(time.time()) - start_time
+	elapsed_seconds = int((int(time.time()) - start_time) / divisor)
 
 	if elapsed_seconds > 60 * 60:
 		hours, minutes = divmod(elapsed_seconds, 60 * 60)
