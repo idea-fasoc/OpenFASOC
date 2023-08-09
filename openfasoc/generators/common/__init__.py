f9367ff16421d1306254eb4410a84f60dd18a413
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-08-10 00:52:19 +0530

    docs: updated docstring for top level common module

--- openfasoc/generators/common/__init__.py
+++ openfasoc/generators/common/__init__.py
@@ -5,6 +5,8 @@ This module exports functions used in OpenFASOC generators. The following submod
 - `common.verilog_generation`
 	1. `generate_verilog(parameters: dict, src_dir: str, out_dir: str) -> None`: Used to generate synthesizable Verilog files (for OpenROAD flow) from source Mako-based Verilog templates.
 	2. `COMMON_PLATFORMS_PREFIX_MAP` (dict): This is a dictionary of common platforms (currently sky130) and their cell naming prefixes.
+- `common.simulation`
+	1. `run_simulations()`: Used to run SPICE testbenches with multiple parameters.
 
 See individual function documentation for more information on a particular function.
 """
