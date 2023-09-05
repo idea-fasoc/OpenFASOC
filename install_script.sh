7376687229fc6b5644de7d4bc5dafa3fb5a2ba89
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-09-05 15:12:04 +0530

    Create install_script.sh

---
+++ install_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install gcc g++
+
+mv docker/conda/scripts/get_conda.sh .
+mv docker/conda/scripts/dependencies.sh .
+
+chmod +x get_conda.sh
+chmod +x dependencies.sh
+
+./get_conda.sh
+sudo ./dependencies.sh
+source ~/.bashrc
