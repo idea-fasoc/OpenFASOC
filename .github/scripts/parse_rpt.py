aae4dae499086c37574d1d7ea8ecf6d041e76251
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-10-11 11:10:43 +0530

    Update parse_rpt.py
    Add checks for .csv files and ../work directory files (.gds, .pex, .v, .spice, etc.)

--- .github/scripts/parse_rpt.py
+++ .github/scripts/parse_rpt.py
@@ -31,3 +31,23 @@ with open(lvs_filename) as f:
         raise ValueError("LVS failed!")
     else:
         print("LVS is clean!")
+
+if sys.argv[1] != "sky130hvl_ldo":
+    with open('test.json', 'r') as file:
+    data = json.load(file)
+
+    module_name = data.get("module_name", "default")
+
+    work_dir = "./work/"
+
+    if (os.path.exists(work_dir) == 0):
+        raise ValueError("work directory does not exist!")
+    else:
+        filename = work_dir + module_name
+        for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
+            if (os.path.exists(file) == 0):
+                raise ValueError(file + " does not exist!")
+            
+    for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
+        if os.path.exists(file) == 0:
+            raise ValueError(file + " does not exist!")
