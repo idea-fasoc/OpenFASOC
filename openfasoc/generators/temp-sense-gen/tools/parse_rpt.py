78dcabba3364453f61b8b88a4fbfba00f9054dc8
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-10-11 11:20:59 +0530

    Update parse_rpt.py
    Added print statements

--- openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
+++ openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
@@ -22,6 +22,7 @@ with open(lvs_filename) as f:
         
 with open('test.json', 'r') as file:
     data = json.load(file)
+print('Found .json config file...')
 
 module_name = data.get("module_name", "default")
 
@@ -34,7 +35,10 @@ else:
     for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
         if (os.path.exists(file) == 0):
             raise ValueError(file + " does not exist!")
-            
+print("Found necessary work result files!")
+
 for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
     if os.path.exists(file) == 0:
         raise ValueError(file + " does not exist!")
+print("Found generated .csv files!")
+print("Generator check is clean!")
