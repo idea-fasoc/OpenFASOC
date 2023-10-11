8ff30e7af8b30816f497770e3ded857c414fa6f0
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-10-11 11:02:03 +0530

    Update parse_rpt.py

--- openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
+++ openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
@@ -1,3 +1,6 @@
+import json
+import os 
+
 drc_filename = "flow/reports/sky130hd/tempsense/6_final_drc.rpt"
 num_lines = sum(1 for line in open(drc_filename))
 
@@ -16,3 +19,22 @@ with open(lvs_filename) as f:
         raise ValueError("LVS failed!")
     else:
         print("LVS is clean!")
+        
+with open('test.json', 'r') as file:
+    data = json.load(file)
+
+module_name = data.get("module_name", "default")
+
+work_dir = "./work/"
+
+if (os.path.exists(work_dir) == 0):
+    raise ValueError("work directory does not exist!")
+else:
+    filename = work_dir + module_name
+    for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
+        if (os.path.exists(file) == 0):
+            raise ValueError(file + " does not exist!")
+            
+for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
+    if os.path.exists(file) == 0:
+        raise ValueError(file + " does not exist!")
