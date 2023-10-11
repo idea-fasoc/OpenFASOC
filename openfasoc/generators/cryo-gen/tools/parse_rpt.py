069f936467e784a86997e692a045555f0674309e
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-10-11 11:08:38 +0530

    Update parse_rpt.py
    Update to include checks for .csv files and ../work directory files

--- openfasoc/generators/cryo-gen/tools/parse_rpt.py
+++ openfasoc/generators/cryo-gen/tools/parse_rpt.py
@@ -22,3 +22,22 @@ if match != None:
     raise ValueError("LVS failed!")
 else:
     print("LVS is clean!")
+
+with open('test.json', 'r') as file:
+    data = json.load(file)
+
+    module_name = data.get("module_name", "default")
+
+    work_dir = "./work/"
+
+    if (os.path.exists(work_dir) == 0):
+        raise ValueError("work directory does not exist!")
+    else:
+        filename = work_dir + module_name
+        for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
+            if (os.path.exists(file) == 0):
+                raise ValueError(file + " does not exist!")
+            
+    for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
+        if os.path.exists(file) == 0:
+            raise ValueError(file + " does not exist!")
