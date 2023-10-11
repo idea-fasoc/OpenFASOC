95eb257d87478ce98805791fe9a0fcb86fcd5e19
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-10-11 11:32:17 +0530

    Update parse_rpt.py
    Added ../work directory and .csv file checks, commented because only the verilog make is supported

--- openfasoc/generators/PMU-gen/tools/parse_rpt.py
+++ openfasoc/generators/PMU-gen/tools/parse_rpt.py
@@ -22,3 +22,26 @@ if match != None:
     raise ValueError("LVS failed!")
 else:
     print("LVS is clean!")
+
+# with open('test.json', 'r') as file:
+#     data = json.load(file)
+# print('Found .json config file...')
+
+# module_name = data.get("module_name", "default")
+
+# work_dir = "./work/"
+
+# if (os.path.exists(work_dir) == 0):
+#     raise ValueError("work directory does not exist!")
+# else:
+#     filename = work_dir + module_name
+#     for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
+#         if (os.path.exists(file) == 0):
+#             raise ValueError(file + " does not exist!")
+# print("Found necessary work result files!")
+
+# for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
+#     if os.path.exists(file) == 0:
+#         raise ValueError(file + " does not exist!")
+# print("Found generated .csv files!")
+print("Generator check is clean!")
