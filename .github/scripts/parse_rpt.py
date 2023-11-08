ad891837802d59b5c5c4d99ff103b665f62fd0d6
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-11-08 12:04:35 +0530

    add check for presence of json file

--- .github/scripts/parse_rpt.py
+++ .github/scripts/parse_rpt.py
@@ -50,25 +50,29 @@ else:
 if ((len(sys.argv) > 1) and ((sys.argv[1] == "sky130hvl_ldo") or (sys.argv[1] == "sky130hvl_ldo_full"))) or ((len(sys.argv) > 1) and (sys.argv[1] == "sky130hd_cryo")):
     print("Generator check is clean!")
 else:
-    with open('test.json', 'r') as file:
-    	data = json.load(file)
-    print('Found .json config file...')
-
-    module_name = data.get("module_name", "default")
-
-    work_dir = "./work/"
-
-    if (os.path.exists(work_dir) == 0):
-        raise ValueError("work directory does not exist!")
-    else:
-        filename = work_dir + module_name
-        for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
-            if (os.path.exists(file) == 0):
-                raise ValueError(file + " does not exist!")
-    print("Found necessary work result files!")
+    json_filename = "test.json"
+    if os.path.exists(json_filename):
+        with open(json_filename) as file:
+        	data = json.load(file)
+        print('Found .json config file...')
     
-    for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
-        if os.path.exists(file) == 0:
-            raise ValueError(file + " does not exist!")
-    print("Found generated .csv files!")
-    print("Generator check is clean!")
+        module_name = data.get("module_name", "default")
+    
+        work_dir = "./work/"
+    
+        if (os.path.exists(work_dir) == 0):
+            raise ValueError("work directory does not exist!")
+        else:
+            filename = work_dir + module_name
+            for file in (filename + ".gds", filename + ".spice", filename + ".v", filename + ".def", filename + "_pex.spice", filename + ".sdc"):
+                if (os.path.exists(file) == 0):
+                    raise ValueError(file + " does not exist!")
+        print("Found necessary work result files!")
+        
+        for file in ("error_within_x.csv", "golden_error_opt.csv", "search_result.csv"):
+            if os.path.exists(file) == 0:
+                raise ValueError(file + " does not exist!")
+        print("Found generated .csv files!")
+        print("Generator check is clean!")
+    else:
+        raise ValueError(".json config file not found!")
