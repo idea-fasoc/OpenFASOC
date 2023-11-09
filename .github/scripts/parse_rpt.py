1c838ed9de3e449bcc869ed9d88978fb0859c456
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-11-09 15:19:34 +0530

    Add simulation tests in parse_rpt.py

--- .github/scripts/parse_rpt.py
+++ .github/scripts/parse_rpt.py
@@ -31,3 +31,19 @@ with open(lvs_filename) as f:
         raise ValueError("LVS failed!")
     else:
         print("LVS is clean!")
+
+if len(sys.argv) > 1 and sys.argv[1] == "sky130hd_temp_full":
+    result_filename = "work/prePEX_sim_result"
+    sim_state_filename = "work/sim_state_file.txt"
+
+    with open(result_filename) as f2, open("../../../.github/scripts/expected_sim_outputs/prePEX_sim_result.txt") as f1:
+        content1 = f2.readlines()
+        content2 = f1.readlines()
+        if content1 != content2:
+            raise ValueError("Simulations failed: simulation result file does not match!")
+    
+    sim_state = json.load(open("work/sim_state_file.txt"))
+    if sim_state["failed_sims"] != 0:
+        raise ValueError("Simulations failed: Non zero failed simulations!")
+    
+    print("Simulations are clean!")
