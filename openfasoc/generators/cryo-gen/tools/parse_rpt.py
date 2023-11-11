ba5674ca25195b10bb5b6d8560efb0f8fdb7e70d
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-11-11 19:04:50 +0530

    Make all updates for modularity

--- openfasoc/generators/cryo-gen/tools/parse_rpt.py
+++ openfasoc/generators/cryo-gen/tools/parse_rpt.py
@@ -5,10 +5,17 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from common.check_gen_files import check_gen_files
 
+_generator_is = {
+    'sky130hvl_ldo': 0, 
+    'sky130hd_temp': 0, 
+    'sky130XX_cryo': 1
+}
+
 dir_path = r'flow/reports'
 lib = os.listdir(dir_path)
+cryo_library = str(lib[0])
 
-drc_filename = "flow/reports/" + str(lib[0]) + "/cryo/6_final_drc.rpt"
+drc_filename = "flow/reports/" + cryo_library + "/cryo/6_final_drc.rpt"
 num_lines = sum(1 for line in open(drc_filename))
 
 if num_lines > 3:
@@ -16,7 +23,7 @@ if num_lines > 3:
 else:
     print("DRC is clean!")
 
-lvs_filename = "flow/reports/" + str(lib[0]) + "/cryo/6_final_lvs.rpt"
+lvs_filename = "flow/reports/" + cryo_library + "/cryo/6_final_lvs.rpt"
 lvs_line = subprocess.check_output(["tail", "-1", lvs_filename]).decode(
     sys.stdout.encoding
 )
@@ -31,7 +38,7 @@ else:
 
 json_filename = "test.json"
 
-if check_gen_files(json_filename, (len(sys.argv) == 1)):
+if check_gen_files(json_filename, _generator_is, cryo_library):
         print("Flow check is clean!")
 else:
     print("Flow check failed!")
