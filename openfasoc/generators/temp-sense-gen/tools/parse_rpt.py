889a4aa1e70353886e34d0ab43e2d20e47ee3cf9
Chetanya <95761876+chetanyagoyal@users.noreply.github.com>
2023-11-11 15:17:30 +0530

    update file_paths to follow convention

--- openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
+++ openfasoc/generators/temp-sense-gen/tools/parse_rpt.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from common.check_gen_files import check_gen_files
 
-drc_filename = "flow/reports/sky130hd/tempsense/6_final_drc.rpt"
+drc_filename = "work/6_final_drc.rpt"
 num_lines = sum(1 for line in open(drc_filename))
 
 if num_lines > 3:
@@ -12,7 +12,7 @@ else:
     print("DRC is clean!")
 
 
-lvs_filename = "flow/reports/sky130hd/tempsense/6_final_lvs.rpt"
+lvs_filename = "work/6_final_lvs.rpt"
 
 with open(lvs_filename) as f:
     f1 = f.read()
