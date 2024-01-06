3e25809cc3916f1ff61ec0b7b26f51050d0d71d4
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-01-06 17:42:34 +0530

    fix: removed pmos_2 from ldo_custom_net.txt

--- openfasoc/generators/ldo-gen/tools/generate_verilog.py
+++ openfasoc/generators/ldo-gen/tools/generate_verilog.py
@@ -49,10 +49,12 @@ def update_custom_nets(blocksDir, arrSize):
     """Creates custom routes in ldo_custom_net.txt."""
     with open(blocksDir + "/ldo_custom_net.txt", "w") as ldo_domain_insts:
         # Always write comparator and pmos connections
-        if arrSize > INCLUDE_2_PMOS_ARRSIZE:
-            ldo_domain_insts.write("r_VREG\ncmp1 VREG\npmos_1 VREG\npmos_2 VREG\n")
-        else:
-            ldo_domain_insts.write("r_VREG\ncmp1 VREG\npmos_1 VREG\n")
+        ldo_domain_insts.write("r_VREG\ncmp1 VREG\npmos_1 VREG\n")
+
+        # The below code is commented since pmos_2 is commented in the Verilog
+        # if arrSize > INCLUDE_2_PMOS_ARRSIZE:
+        #     ldo_domain_insts.write("pmos_2 VREG\n")
+
         # write arrSize pt cells
         for i in range(arrSize):
             ldo_domain_insts.write("{pt_array_unit\[" + str(i) + "\]} VREG\n")
@@ -80,8 +82,8 @@ def update_area_and_place_density(flowDir, arrSize):
     core_length = core_width = 260 + 20 * int(arrSize / 50)
     vreg_width = die_width - 39
 
-    # place_density = round(0.3 + 0.1 * math.ceil((arrSize%50)/10),1)
-    place_density = 0.426
+    place_density = round(0.3 + 0.1 * math.ceil((arrSize%50)/10),1)
+    # place_density = 0.426
 
     config_template = config_template.replace("@PARAM_DIE_WIDTH", str(die_width), 1)
     config_template = config_template.replace("@PARAM_DIE_LENGTH", str(die_length), 1)
