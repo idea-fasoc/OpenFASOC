477ff15663e0edcebde4221a0786e8e570061a2a
tsengs0 <tsengs0@gmail.com>
2024-11-14 17:13:19 +0000

    Add functions to automatically insert the external I/O ports on the target Pcell

--- openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/cell_config.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/cell_config.py
@@ -15,7 +15,25 @@ from glayout.flow.placement.two_transistor_interdigitized import two_nfet_interd
 from glayout.flow.placement.two_transistor_interdigitized import two_pfet_interdigitized
 from glayout.flow.pdk.util.comp_utils import prec_ref_center, movey, evaluate_bbox, align_comp_to_port
 
-def add_port_lvs(pdk: MappedPDK, comp: Component, port_list: list[ dict[str, Union[float, str]] ]) -> Component:
+def add_wire(comp, xsize=2, ysize=1, layer=(68, 20), label="new_wire", center=(0, 0), position_offset=(1.4, 1.4)):
+	c = Component()
+	c.add_polygon(
+		[
+			(center[0]-position_offset[0], center[1]-position_offset[1]),
+			(center[0]+position_offset[0], center[1]-position_offset[1]),
+			(center[0]+position_offset[0], center[1]-position_offset[1]-ysize),
+			(center[0]-position_offset[0], center[1]-position_offset[1]-ysize)
+		],
+		layer=layer
+	)
+	comp.add_port(name=f"{label}_W", center=[0, ysize / 2], width=ysize, orientation=0, layer=layer)
+	comp.add_port(name=f"{label}_E", center=[xsize, ysize / 2], width=ysize, orientation=0, layer=layer)
+	c.add_label(text=f"{label}_W", position=(c.x-xsize/2, c.y), layer=layer)
+	c.add_label(text=f"{label}_E", position=(c.x+xsize/2, c.y), layer=layer)
+	
+	return c
+
+def add_port_lvs(pdk: MappedPDK, comp: Component, port_list: list[ dict[str, Union[float, str]] ], port_feature: dict[str, str]) -> Component:
 	'''
     To add external I/O ports onto the cell for LVS
 
@@ -41,27 +59,58 @@ def add_port_lvs(pdk: MappedPDK, comp: Component, port_list: list[ dict[str, Uni
 								"ref_port": "multiplier_0_source_E"
 							}
 						)
+		# port_feature: a "dict"-type primitive specifying the feature of the new port, e.g.
+						{"port_type":"pin", "layer":None} -> To create a rectangle as the new port
+						{"port_type":"wire", "layer":"met1"} -> To extend the existing port by a wire only
     
 		# return: gdsfactory.Component
 
 	@ Limitations:
 		# So far, only skywater130 process is validated
 	'''
+	
+	if port_feature["port_type"] == "pin":
+		port_type = 0
+	elif port_feature["port_type"] == "wire":
+		new_port_layer = pdk.get_glayer(port_feature["layer"])
+		port_type = 1
+		wire_width = comp.xsize
+		wire_height = pdk.get_grule(port_feature["layer"])["min_width"]
+		new_port_center = comp.center
+		new_port_position_offset = (comp.xsize/2, comp.ysize/2+pdk.util_max_metal_seperation())
+		wire_spacing = pdk.get_grule(port_feature["layer"])["min_separation"]+wire_height
+		port_cnt = 0
+	else:
+		raise ValueError("The port type must be either pin or wire")
 
-	# Add pins and text labels for LVS
-	pins_labels_info = list() # list that contains all port and component information
-	print(f"port_list: {port_list}")
-	for port in port_list:
-		# To get the layer's data type mapped to the GDS
-		ref_port_layer = comp.ports[ port["ref_port"] ].layer[0] # [0]: layer mapping, [1]: pin, drawing, label, net, etc.
+	if port_type == 0: # port of which features pin-added type
+		# Add pins and text labels for LVS
+		pins_labels_info = list() # list that contains all port and component information
+		print(f"port_list: {port_list}")
+		for port in port_list:
+			# To get the layer's data type mapped to the GDS
+			ref_port_layer = comp.ports[ port["ref_port"] ].layer[0] # [0]: layer mapping, [1]: pin, drawing, label, net, etc.
 
-		# To create the pin w/ label where the layer[1] is mapped to 16
-		new_port_pin = rectangle(layer=(ref_port_layer, 16), size=(port["pin_width"], port["pin_height"]),centered=True).copy() # True set rectangle's centroid to the relative (0, 0)
-		new_port_pin.add_label(text=port["new_port_label"], layer=(ref_port_layer, 5)) # layer[1]=5 mapped to the "label" datatype in the GDS
+			# To create the pin w/ a label where the layer[1] is mapped to 16
+			new_port = rectangle(layer=(ref_port_layer, 16), size=(port["pin_width"], port["pin_height"]),centered=True).copy() # True set rectangle's centroid to the relative (0, 0)
+			new_port.add_label(text=port["new_port_label"], layer=(ref_port_layer, 5)) # layer[1]=5 mapped to the "label" datatype in the GDS
 
-		# To align the new port with the designated port existing in the given component
-		alignment = ('c', 'b')
-		comp_ref = align_comp_to_port(new_port_pin, comp.ports[ port["ref_port"] ], alignment=alignment)
-		comp.add(comp_ref)
+			# To align the new port with the designated port existing in the given component
+			alignment = ('c', 'b')
+			comp_ref = align_comp_to_port(new_port, comp.ports[ port["ref_port"] ], alignment=alignment)
+			comp.add(comp_ref)
+
+	else: # port_type=1, i.e. port of which features wire_only
+		# Add pins and text labels for LVS
+		pins_labels_info = list() # list that contains all port and component information
+		print(f"port_list: {port_list}")
+		for port in port_list:
+			# To create the pin where the layer[1] is mapped to 16
+			new_port = add_wire(comp=comp, xsize=wire_width, ysize=wire_height, layer=new_port_layer, label=port["new_port_label"], center=new_port_center, position_offset=new_port_position_offset)
+			new_port_ref = comp.add_ref(new_port)
+			new_port_ref.movey(-wire_spacing*port_cnt)
+			comp << smart_route(pdk, comp.ports.get(port["ref_port"]), comp.ports.get(port["new_port"]+"_E"))
+			
+			port_cnt = port_cnt + 1 # Increment the index indicating the new port
 
 	return comp
--- openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/eval.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/eval.py
@@ -10,31 +10,32 @@ def main():
         comp=tg_inst,
         port_list=[
 	    	{
-                "new_port": "tg_ctrl", 
-	    		"new_port_label": "ctrl",
+                "new_port": "Vctrl", 
+	    		"new_port_label": "Vctrl",
 	    		"pin_width": tg_inst.ports["inv_nmos_multiplier_0_gate_S"].width, 
 	    		"pin_height": tg_inst.ports["inv_nmos_multiplier_0_gate_S"].width, 
 	    		"ref_port": "inv_nmos_multiplier_0_gate_S"
             },
 	    	{
-                "new_port": "tg_din", 
-	    		"new_port_label": "din",
+                "new_port": "Vin", 
+	    		"new_port_label": "Vin",
 	    		"pin_width": 0.3,#tg_inst.ports["tg_nmos_multiplier_0_source_S"].width, 
 	    		"pin_height": 0.3,#tg_inst.ports["tg_nmos_multiplier_0_source_S"].width, 
 	    		"ref_port": "tg_pmos_multiplier_0_source_W"
             },
 	    	{
-                "new_port": "tg_dout", 
-	    		"new_port_label": "dout",
+                "new_port": "Vout", 
+	    		"new_port_label": "Vout",
 	    		"pin_width": 0.3,#tg_inst.ports["tg_nmos_multiplier_0_drain_S"].width, 
 	    		"pin_height": 0.3,#tg_inst.ports["tg_nmos_multiplier_0_drain_S"].width, 
 	    		"ref_port": "tg_nmos_multiplier_0_drain_N"
             }
-	    ]
+	    ],
+		port_feature = {"port_type":"wire", "layer":"met2"} # met2 is actually mapped to met1 in sky130 proceess
 	)
     #tg_inst.flatten()
     tg_inst.show()
-    tg_inst.write_gds("gds/tg_with_inv.gds")
+	#tg_inst.write_gds("gds/tg_with_inv.gds")
 
 if __name__ == "__main__":
     main()
