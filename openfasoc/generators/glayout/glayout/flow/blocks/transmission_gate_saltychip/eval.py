c75a0a5540f14fad6c478eb8c7f1c224d034831e
tsengs0 <tsengs0@gmail.com>
2024-11-17 14:45:27 +0000

    Team: Salty Chip, Chipathon2024     Fix the DRC error by removing the deep N-well on the NMOS of the inv cell which     is used as the control component for the underlying transmission gate

--- openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/eval.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/eval.py
@@ -1,43 +1,29 @@
 import transmission_gate as tg
 #from glayout.flow.pdk.gf180_mapped import gf180
 from glayout.flow.pdk.sky130_mapped import sky130_mapped_pdk as sky130
-import cell_config as config
+import reconfig_inv as reconfig_inv
 
 TARGET_PDK = sky130
 
 def main():
-    tg_inst = tg.tg_with_inv(pdk=TARGET_PDK, pmos_width=1, pmos_length=0.15, nmos_width=1, nmos_length=0.15)
-    tg_inst = config.add_port_lvs(
-        pdk=sky130,
-        comp=tg_inst,
-        port_list=[
-	    	{
-                "new_port": "Vctrl", 
-	    		"new_port_label": "Vctrl",
-				"new_port_via": True,
-				"new_port_via_layers": ("met1", "met2"),
-				"new_port_move": (0, -TARGET_PDK.get_grule("mcon")["min_separation"]),
-	    		"ref_port": "inv_nmos_multiplier_0_gate_E",
-				"ref_port_align": ("c", "b"),
-				"connect_pos": "W" # To connce the ref_port to the "west" side of the new port
-            }#,
-	    	#{
-            #    "new_port": "Vin", 
-	    	#	"new_port_label": "Vin", 
-	    	#	"ref_port": "tg_pmos_multiplier_0_source_W"
-            #},
-	    	#{
-            #    "new_port": "Vout", 
-	    	#	"new_port_label": "Vout",
-	    	#	"ref_port": "tg_nmos_multiplier_0_drain_W",
-			#	"connect_pos": "E" # To connect the ref_port to the "east" side of the new port
-            #}
-	    ],
-		port_feature = {"port_type":"pin", "layer":"met1"} # met1 is actually mapped to li1 in sky130 proceess
+	gate_ctrl_inv = reconfig_inv.reconfig_inv(
+		pdk=TARGET_PDK,
+		component_name="gate_ctrl_inv",
+		pmos_width=1,
+		pmos_length=0.15,
+		nmos_width=1,
+		nmos_length=0.15,
+		orientation="horizontal"
 	)
-    #tg_inst.flatten()
-    tg_inst.show()
-	#tg_inst.write_gds("gds/tg_with_inv.gds")
+	gate_ctrl_inv.show()
+	gate_ctrl_inv.write_gds("/home/tsengs0/gate_ctrl_inv.gds")
+
+	magic_drc_result = sky130.drc_magic(
+		layout=gate_ctrl_inv,
+		design_name=gate_ctrl_inv.name#,
+		#output_file=f"{absolute_path}/{gate_ctrl_inv.name}.rpt"
+	)
+	print("Magic DRC result: \n", magic_drc_result)
 
 if __name__ == "__main__":
     main()
--- openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/transmission_gate.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/transmission_gate.py
@@ -16,94 +16,19 @@ from glayout.flow.placement.two_transistor_interdigitized import two_pfet_interd
 from glayout.flow.pdk.util.comp_utils import prec_ref_center, movey, evaluate_bbox, align_comp_to_port
 
 # My own cell library
-from inv_lib import reconfig_inv
-import cell_config as config
-
-'''
-def naive_tg_cell(pdk: MappedPDK, pmos_width, pmos_length, nmos_width, nmos_length):
-	# To prepare all necessary cells to construct a transmission gate, i.e.
-	# 1) PMOS
-	# 2) NMOS
-	pfet = pmos(pdk=pdk, with_substrate_tap=False, with_dummy=(False, False), width=pmos_width, length=pmos_length)
-	nfet = nmos(pdk=pdk, with_substrate_tap=False, with_dummy=(False, False), width=nmos_width, length=nmos_length)
-
-	# Placement and adding ports
-	top_level = Component(name="TG")
-	pfet_ref = prec_ref_center(pfet)
-	nfet_ref = prec_ref_center(nfet)
-	top_level.add(pfet_ref)
-	top_level.add(nfet_ref)
-	top_level.add_ports(pfet_ref.get_ports_list(), prefix="pmos_")
-	top_level.add_ports(nfet_ref.get_ports_list(), prefix="nmos_")
-
-	# Placement
-	mos_spacing = pdk.util_max_metal_seperation()
-	#mos_spacing = pdk.get_grule("met1")["min_width"])
-	pfet_ref.rotate(90)
-	nfet_ref.rotate(90)
-	pfet_ref.movey(evaluate_bbox(nfet)[1] + mos_spacing)
-
-	# Routing
-	top_level << smart_route(pdk, pfet_ref.ports["multiplier_0_source_W"], nfet_ref.ports["multiplier_0_drain_W"]) # "in" of the TG
-	top_level << smart_route(pdk, pfet_ref.ports["multiplier_0_drain_W"], nfet_ref.ports["multiplier_0_source_W"]) # "out" of the TG
-
-	# Add pins and text labels for LVS
-	pins_labels_info = list() # list that contains all port and component information
-	# To define the layers
-	gds_met1 = pdk.get_glayer("met2")[0]
-	gds_met2 = gds_met1+1
-	gds_met3 = gds_met2+1
-	gds_met4 = gds_met3+1
-	gds_met5 = gds_met4+1
-	# To get the respective layers of the underlying TG's PMOS.source port PMOS.drain port
-	tg_din_portLayer = top_level.ports["pmos_multiplier_0_source_W"].layer[0]
-	tg_dout_portLayer = top_level.ports["pmos_multiplier_0_drain_E"].layer[0]
-	# To create the pins w/ labels and append to info list
-	tg_din_pin = rectangle(layer=(tg_din_portLayer, 16), size=(1, 1),centered=True).copy() # True set rectangle's centroid to the relative (0, 0)
-	tg_dout_pin = rectangle(layer=(tg_dout_portLayer, 16), size=(1, 1), centered=True).copy()
-	tg_din_pin.add_label(text="Vin", layer=(tg_din_portLayer, 5))
-	tg_dout_pin.add_label(text="Vout", layer=(tg_dout_portLayer, 5))
-	pins_labels_info.append((tg_din_pin, top_level.ports["pmos_multiplier_0_source_W"], None))
-	pins_labels_info.append((tg_dout_pin, top_level.ports["pmos_multiplier_0_drain_E"], None))
-
-	#print(top_level.ports["pmos_multiplier_0_source_W"].name)
-	#top_level.pprint_ports()
-
-	# Move everythin to position
-	for comp, prt, alignment in pins_labels_info:
-		alignment = ('c', 'b') if alignment is None else alignment
-		compref = align_comp_to_port(comp, prt, alignment=alignment)
-		#top_level.add(compref)
-
-	return top_level #top_level.flatten()
-
-def tg_with_inv(pdk: MappedPDK, pmos_width, pmos_length, nmos_width, nmos_length):
-	# To prepare all necessary cells to construct a transmission gate, i.e.
-	# 1) transmission gate
-	# 2) Inverter
-	tg = naive_tg_cell(pdk=pdk, pmos_width=pmos_width, pmos_length=pmos_length, nmos_width=nmos_width, nmos_length=nmos_length)
-	inv = reconfig_inv(pdk=pdk, component_name="gate_ctrl_inv", pmos_width=pmos_width, pmos_length=pmos_length, nmos_width=nmos_width, nmos_length=nmos_length, orientation="horizontal")
-
-	# Placement and adding ports
-	top_level = Component(name="tg_with_inv")
-	tg_ref = prec_ref_center(tg)
-	inv_ref = prec_ref_center(inv)
-	top_level.add(tg_ref)
-	top_level.add(inv_ref)
-	top_level.add_ports(tg_ref.get_ports_list(), prefix="tg_")
-	top_level.add_ports(inv_ref.get_ports_list(), prefix="inv_")
-
-	# Placement
-	mos_spacing = pdk.util_max_metal_seperation()
-	tg_ref.movex(evaluate_bbox(inv)[0] + mos_spacing)
-
-	# Routing
-	#top_level << smart_route(pdk, pfet_ref.ports["multiplier_0_source_W"], nfet_ref.ports["multiplier_0_drain_W"]) # "in" of the TG
-	#top_level << smart_route(pdk, pfet_ref.ports["multiplier_0_drain_E"], nfet_ref.ports["multiplier_0_source_E"]) # "out" of the TG
-
-	return top_level #top_level.flatten()
-'''
-def naive_tg_cell(pdk: MappedPDK, flip_config: dict[str, Union[int, str]], pmos_width, pmos_length, nmos_width, nmos_length):
+from reconfig_inv import reconfig_inv
+	
+#@cell
+def tg_cell(
+	pdk: MappedPDK,
+	flip_config:
+	dict[str, Union[int, str]],
+	pmos_width,
+	pmos_length,
+	nmos_width,
+	nmos_length,
+	**kwargs
+) -> Component:
 	# To prepare all necessary cells to construct a transmission gate, i.e.
 	# 1) PMOS
 	# 2) NMOS
@@ -111,7 +36,7 @@ def naive_tg_cell(pdk: MappedPDK, flip_config: dict[str, Union[int, str]], pmos_
 	nfet = nmos(pdk=pdk, with_substrate_tap=False, with_dummy=(False, True), width=nmos_width, length=nmos_length)
 
 	# Placement and adding ports
-	top_level = Component(name="TG")
+	top_level = Component(name="tg_1b")
 	pfet_ref = prec_ref_center(pfet)
 	nfet_ref = prec_ref_center(nfet)
 	top_level.add(pfet_ref)
@@ -119,7 +44,6 @@ def naive_tg_cell(pdk: MappedPDK, flip_config: dict[str, Union[int, str]], pmos_
 
 	# Placement
 	mos_spacing = pdk.util_max_metal_seperation()
-	#mos_spacing = pdk.get_grule("met1")["min_width"])
 	if flip_config["degree"] != None:
 		pfet_ref.rotate(flip_config["degree"])
 		nfet_ref.rotate(flip_config["degree"])
@@ -130,49 +54,19 @@ def naive_tg_cell(pdk: MappedPDK, flip_config: dict[str, Union[int, str]], pmos_
 	#     a) PMOS.source connected to NMOS.source
 	#     b) PMOS.drain connected to NMOS.drain 
 	top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_source_E"], nfet_ref.ports["multiplier_0_drain_E"], glayer1="met2") # "in" of the TG
-	top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_drain_W"], nfet_ref.ports["multiplier_0_source_E"], glayer1="met1") # "out" of the TG
+	top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_drain_W"], nfet_ref.ports["multiplier_0_source_E"], glayer1="met3") # "out" of the TG
 
 	top_level.add_ports(pfet_ref.get_ports_list(), prefix="pmos_")
 	top_level.add_ports(nfet_ref.get_ports_list(), prefix="nmos_")
-	#top_level.add_port(
-	#	name="", center=[0, width / 2], width=width, orientation=180, layer=layer
-	#)
 
-	# Add pins and text labels for LVS
-	pins_labels_info = list() # list that contains all port and component information
-	# To define the layers
-	gds_met1 = pdk.get_glayer("met2")[0]
-	gds_met2 = gds_met1+1
-	gds_met3 = gds_met2+1
-	gds_met4 = gds_met3+1
-	gds_met5 = gds_met4+1
-	# To get the respective layers of the underlying TG's PMOS.source port PMOS.drain port
-	tg_din_portLayer = top_level.ports["pmos_multiplier_0_source_W"].layer[0]
-	tg_dout_portLayer = top_level.ports["pmos_multiplier_0_drain_E"].layer[0]
-	# To create the pins w/ labels and append to info list
-	tg_din_pin = rectangle(layer=(tg_din_portLayer, 16), size=(1, 1),centered=True).copy() # True set rectangle's centroid to the relative (0, 0)
-	tg_dout_pin = rectangle(layer=(tg_dout_portLayer, 16), size=(1, 1), centered=True).copy()
-	tg_din_pin.add_label(text="Vin", layer=(tg_din_portLayer, 5))
-	tg_dout_pin.add_label(text="Vout", layer=(tg_dout_portLayer, 5))
-	pins_labels_info.append((tg_din_pin, top_level.ports["pmos_multiplier_0_source_W"], None))
-	pins_labels_info.append((tg_dout_pin, top_level.ports["pmos_multiplier_0_drain_E"], None))
+	return top_level
 
-	#print(top_level.ports["pmos_multiplier_0_source_W"].name)
-	#top_level.pprint_ports()
-
-	# Move everythin to position
-	for comp, prt, alignment in pins_labels_info:
-		alignment = ('c', 'b') if alignment is None else alignment
-		compref = align_comp_to_port(comp, prt, alignment=alignment)
-		#top_level.add(compref)
-
-	return top_level #top_level.flatten()
-
-def tg_with_inv(pdk: MappedPDK, pmos_width, pmos_length, nmos_width, nmos_length):
+#@cell
+def tg_with_ctrl(pdk: MappedPDK, pmos_width, pmos_length, nmos_width, nmos_length):
 	# To prepare all necessary cells to construct a transmission gate, i.e.
 	# 1) transmission gate
 	# 2) Inverter
-	tg = naive_tg_cell(pdk=pdk, flip_config={"degree": 270}, pmos_width=pmos_width, pmos_length=pmos_length, nmos_width=nmos_width, nmos_length=nmos_length)
+	tg = tg_cell(pdk=pdk, flip_config={"degree": 270}, pmos_width=pmos_width, pmos_length=pmos_length, nmos_width=nmos_width, nmos_length=nmos_length)
 	inv = reconfig_inv(pdk=pdk, component_name="gate_ctrl_inv", pmos_width=pmos_width, pmos_length=pmos_length, nmos_width=nmos_width, nmos_length=nmos_length, orientation="horizontal")
 
 	# Instantiation of the essential cells
