0fcd05adeefd41ebbed945483b1cb5e3edcf9cf1
tsengs0 <tsengs0@gmail.com>
2024-11-24 19:20:06 +0000

    Contributor: Team SaltyChip, Chipathon 2024     Add the directory of building the layout of transmission-gate PCell     with GDS, Magic DRC and LVS results

--- openfasoc/generators/glayout/glayout/flow/blocks/elementary/transmission_gate/transmission_gate.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/elementary/transmission_gate/transmission_gate.py
@@ -20,26 +20,7 @@ from glayout.flow.primitives.via_gen import via_stack
 from gdsfactory.cell import cell
 from glayout.flow.spice import Netlist
 from glayout.flow.pdk.util.snap_to_grid import component_snap_to_grid
-
-# My own cell library
-from reconfig_inv import reconfig_inv
-import comp_dc
-
-'''
-def tg_netlist(pmos: Component, nmos: Component) -> Netlist:
-	# A: tg.input, i.e. PMOS & NMOS source
-	# Y: tg.output, i.e. PMOS & NMOS drain
-	# C: connected to tg.nmos.gate
-	# CBAR: connected to tg.pmos.gate
-	# VDD: connected to tg.pmos.body
-	# VSS: connected to tg.nmos.body
-	netlist = Netlist(circuit_name='tg', nodes=['VDD', 'VSS', 'A', 'Y', 'C', 'CBAR'])
-	#netlist.connect_netlist(pmos.info['netlist'], [('D', 'Y'), ('G', 'CBAR'), ('S', 'A'), ('PB', 'VDD')])
-	#netlist.connect_netlist(nmos.info['netlist'], [('D', 'Y'), ('G', 'C'), ('S', 'A'), ('NB', 'VSS')])
-	netlist.connect_netlist(pmos.info['netlist'], [('D', 'Y'), ('G', 'CBAR'), ('S', 'A')])
-	netlist.connect_netlist(nmos.info['netlist'], [('D', 'Y'), ('G', 'C'), ('S', 'A')])
-	return netlist
-'''
+from glayout.flow.primitives.guardring import tapring
 
 def transmission_gate_netlist(
 	pdk: MappedPDK,
@@ -57,8 +38,8 @@ def transmission_gate_netlist(
 	model_nmos = pdk.models['nfet']
 	print(f"model_pmos: {model_pmos}")
 	source_netlist = """.subckt {circuit_name} {nodes} """ + f'l={length} w={width} m={mtop} ' + """
-XM1 Y CBAR A {model_pmos} l={{l}} w={{w}} m={{m}}
-XM2 Y C A {model_nmos} l={{l}} w={{w}} m={{m}}"""
+XM1 Y C A VDD {model_pmos} l={{l}} w={{w}} m={{m}}
+XM2 Y CBAR A VSS {model_nmos} l={{l}} w={{w}} m={{m}}"""
 	source_netlist += "\n.ends {circuit_name}"
 
 	instance_format = "X{name} {nodes} {circuit_name} l={length} w={width} m={mult}"
@@ -77,16 +58,16 @@ XM2 Y C A {model_nmos} l={{l}} w={{w}} m={{m}}"""
    		}
 	)
 
-@cell
+#@cell
 def short_width_tg(
 	pdk: MappedPDK,
-	component_name: str,
-	orientation_config:
-	dict[str, Union[int, str]],
-	pmos_width,
-	pmos_length,
-	nmos_width,
-	nmos_length,
+	component_name: str = "tg",
+	with_substrate_tap: bool = False,
+	fet_min_width: float = 3,
+	pmos_width: float = 3,
+	pmos_length: float = 0.15,
+	nmos_width: float = 3,
+	nmos_length: float = 0.15,
 	add_pin: bool = True, # For LVS
 	**kwargs
 ) -> Component:
@@ -94,36 +75,117 @@ def short_width_tg(
 	# To prepare all necessary cells to construct a transmission gate, i.e.
 	# 1) PMOS
 	# 2) NMOS
-	pfet = pmos(pdk=pdk, gate_rmult=2, with_tie=False, with_substrate_tap=False, with_dummy=(True, False), width=pmos_width, length=pmos_length)
-	nfet = nmos(pdk=pdk, gate_rmult=2, with_tie=False, with_dnwell=False, with_substrate_tap=False, with_dummy=(False, True), width=nmos_width, length=nmos_length)
-
-	# Placement and adding ports
+	pfet = pmos(pdk=pdk, gate_rmult=1, with_tie=False, with_substrate_tap=False, with_dummy=(True, True), width=pmos_width, length=pmos_length)
+	nfet = nmos(pdk=pdk, gate_rmult=1, with_tie=False, with_dnwell=False, with_substrate_tap=False, with_dummy=(True, True), width=nmos_width, length=nmos_length)
 	top_level = Component(name=component_name)
 	pfet_ref = prec_ref_center(pfet)
 	nfet_ref = prec_ref_center(nfet)
 	top_level.add(pfet_ref)
 	top_level.add(nfet_ref)
 
-	# Placement
+	# Placement (1)
+	#  a) To vertically flip the NMOS such that the its gate point toward the PMOS's gate
+	#  b) To move the PMOS above the NMOS
 	mos_spacing = pdk.util_max_metal_seperation()
-	if orientation_config["degree"] != None:
-		pfet_ref.rotate(orientation_config["degree"])
-		nfet_ref.rotate(orientation_config["degree"])
+	rename_ports_by_orientation(nfet_ref.mirror_y())
 	pfet_ref.movey(evaluate_bbox(nfet)[1] + mos_spacing)
-
-	# Routing
-	# To simplify the routing for the parallel-gate transistors, the layout is realised as follow which is expected to be equivalent to a TG
-	#     a) PMOS.source connected to NMOS.source
-	#     b) PMOS.drain connected to NMOS.drain 
-	top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_source_E"], nfet_ref.ports["multiplier_0_drain_E"], glayer1="met2") # "in" of the TG
-	top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_drain_E"], nfet_ref.ports["multiplier_0_source_E"], glayer1="met2") # "out" of the TG
 
 	# Add the ports aligned with the basic PMOS and NMOS
 	top_level.add_ports(pfet_ref.get_ports_list(), prefix="pmos_")
 	top_level.add_ports(nfet_ref.get_ports_list(), prefix="nmos_")
+	
+	# Placement (2):
+	#  a) Add a bunch of met1-to-met2 vias attached to the mcron-layered points inside the source of PMOS/NMOS
+	#  b) Add a bunch of met1-to-met2 vias attached to the mcron-layered points inside the drain of PMOS/NMOS
+	fet_source_finger_num = 1
+	fet_drain_finger_num = 1
+	finger_col_num = 1
+	finger_row_num = math.floor((fet_min_width*0.5) / (2*pdk.get_grule("met2")["min_width"]))-1 # To pave the stacked vias over all bottom half
+	pmos_source_sdvias = list()
+	nmos_source_sdvias = list()
+	pmos_drain_sdvias = list()
+	nmos_drain_sdvias = list()
+	pmos_source_sdvias_ref = list()
+	nmos_source_sdvias_ref = list()
+	pmos_drain_sdvias_ref = list()
+	nmos_drain_sdvias_ref = list()
+	sdvias_row = [via_stack(pdk=pdk, glayer1="met2", glayer2="met3", fullbottom=True, fulltop=True) for i in range(finger_col_num)]
+	for row in range(finger_row_num):
+		pmos_source_sdvias.append(sdvias_row)
+		nmos_source_sdvias.append(sdvias_row)
+		pmos_drain_sdvias.append(sdvias_row)
+		nmos_drain_sdvias.append(sdvias_row)
 
+		pmos_source_cols_ref = list()
+		nmos_source_cols_ref = list()
+		pmos_drain_cols_ref = list()
+		nmos_drain_cols_ref = list()
+		for col in range(finger_col_num):
+			# For source terminals of the PMOS and NMOS
+			temp_ref = prec_ref_center(pmos_source_sdvias[row][col])
+			pmos_source_cols_ref.append(temp_ref)
+			temp_ref = prec_ref_center(nmos_source_sdvias[row][col])
+			nmos_source_cols_ref.append(temp_ref)
+			# For drain terminals of the PMOS and NMOS
+			temp_ref = prec_ref_center(pmos_drain_sdvias[row][col])
+			pmos_drain_cols_ref.append(temp_ref)
+			temp_ref = prec_ref_center(nmos_drain_sdvias[row][col])
+			nmos_drain_cols_ref.append(temp_ref)
+
+		pmos_source_sdvias_ref.append(pmos_source_cols_ref)
+		nmos_source_sdvias_ref.append(nmos_source_cols_ref)
+		pmos_drain_sdvias_ref.append(pmos_drain_cols_ref)
+		nmos_drain_sdvias_ref.append(nmos_drain_cols_ref)
+		top_level.add(pmos_source_sdvias_ref[row][col] for col in range(finger_col_num))
+		top_level.add(nmos_source_sdvias_ref[row][col] for col in range(finger_col_num))
+		top_level.add(pmos_drain_sdvias_ref[row][col] for col in range(finger_col_num))
+		top_level.add(nmos_drain_sdvias_ref[row][col] for col in range(finger_col_num))
+
+	# Placement (3): 
+	#  a) Move all the a part of particular stacked vias such that they are connected to the source of PMOS/NMOS
+	#  b) Move all the a part of particular stacked vias such that they are connected to the drain of PMOS/NMOS
+	y_offset = pdk.get_grule("met3")["min_separation"]+pdk.get_grule("met3")["min_width"]
+	x_offset = abs(top_level.ports["pmos_multiplier_0_leftsd_array_row0_col0_bottom_via_N"].center[0]-pfet_ref.center[0])
+	for row in range(finger_row_num):
+		# Moving the positions of the stacked vias associated with the PMOS
+		pmos_source_sdvias_ref[row][0].movex(-x_offset)
+		pmos_source_sdvias_ref[row][0].movey(evaluate_bbox(nfet)[1]+mos_spacing-(y_offset*row))
+		pmos_drain_sdvias_ref[row][0].movex(x_offset)
+		pmos_drain_sdvias_ref[row][0].movey(evaluate_bbox(nfet)[1]+mos_spacing-(y_offset*row))
+		
+		# Moving the positions fo the stacked vias associated with NMOS
+		nmos_source_sdvias_ref[row][0].movex(-x_offset)
+		nmos_source_sdvias_ref[row][0].movey(y_offset*row)
+		nmos_drain_sdvias_ref[row][0].movex(x_offset)
+		nmos_drain_sdvias_ref[row][0].movey(y_offset*row)
+
+	# Routing
+	#   a) PMOS.source connected to NMOS.source by placing a large MET2-layered rectangle
+	#   b) PMOS.drain connected to NMOS.drain by placing a large MET2-layered rectangle
+	print("nmos_source_sdvias_ref.len: ", nmos_source_sdvias_ref)
+	sdvias_connection = Component()
+	sdvias_connection.add_polygon(
+		[
+			(nmos_source_sdvias_ref[0][finger_col_num-1].xmin, nmos_source_sdvias_ref[0][finger_col_num-1].ymin),#leftBottom_pos
+			(pmos_source_sdvias_ref[0][finger_col_num-1].xmin, pmos_source_sdvias_ref[0][finger_col_num-1].ymax),#leftTop_pos
+			(pmos_source_sdvias_ref[0][0].xmax, pmos_source_sdvias_ref[0][0].ymax),#rightTop_pos
+			(nmos_source_sdvias_ref[0][0].xmax, nmos_source_sdvias_ref[0][0].ymin) #rightBottom_pos
+		],
+		layer=pdk.get_glayer("met3")
+	)
+	sdvias_connection.add_polygon(
+		[
+			(nmos_drain_sdvias_ref[0][finger_col_num-1].xmin, nmos_drain_sdvias_ref[0][finger_col_num-1].ymin),#leftBottom_pos
+			(pmos_drain_sdvias_ref[0][finger_col_num-1].xmin, pmos_drain_sdvias_ref[0][finger_col_num-1].ymax),#leftTop_pos
+			(pmos_drain_sdvias_ref[0][0].xmax, pmos_drain_sdvias_ref[0][0].ymax),#rightTop_pos
+			(nmos_drain_sdvias_ref[0][0].xmax, nmos_drain_sdvias_ref[0][0].ymin) #rightBottom_pos
+		],
+		layer=pdk.get_glayer("met3")
+	)
+	sdvias_connection_ref = top_level.add_ref(sdvias_connection)
+
+	# Add pins w/ labels for LVS
 	if add_pin == True:
-		# Add pins w/ labels for LVS
 		top_level.unlock()
 		pin_info = list() # list that contains all port and component information
 		met1_pin=(pdk.get_glayer("met1")[0], 20)
@@ -132,15 +194,15 @@ def short_width_tg(
 		# --- Port: A, i.e. input of the transmission gate
 		A_pin=rectangle(layer=met1_pin, size=port_size, centered=True).copy()
 		A_pin.add_label(text="A", layer=met1_label)
-		pin_info.append((A_pin, top_level.ports.get(f"nmos_drain_S"), None))
+		pin_info.append((A_pin, top_level.ports.get(f"nmos_source_W"), ('r', 't')))
 		# --- Port: Y, i.e. output of the transmission gate
 		Y_pin=rectangle(layer=met1_pin, size=port_size, centered=True).copy()
 		Y_pin.add_label(text="Y", layer=met1_label)
-		pin_info.append((Y_pin, top_level.ports.get(f"pmos_drain_S"), None))
+		pin_info.append((Y_pin, top_level.ports.get(f"nmos_drain_E"), ('l', 't')))
 		# --- Port: C, i.e. gate control to the NMOS
 		C_pin=rectangle(layer=met1_pin, size=port_size, centered=True).copy()
 		C_pin.add_label(text="C", layer=met1_label)
-		pin_info.append((C_pin, top_level.ports.get(f"nmos_gate_N"), None))
+		pin_info.append((C_pin, top_level.ports.get(f"nmos_gate_S"), None))
 		# --- Port: CBAR, i.e. gate control to the PMOS
 		CBAR_pin=rectangle(layer=met1_pin, size=port_size, centered=True).copy()
 		CBAR_pin.add_label(text="CBAR", layer=met1_label)
@@ -152,52 +214,65 @@ def short_width_tg(
 			comp_ref = align_comp_to_port(comp, prt, alignment=alignment)
 			top_level.add(comp_ref)
 
-	component = component_snap_to_grid(rename_ports_by_orientation(top_level))
-	component.info['netlist'] = tg_netlist(pmos=pfet, nmos=nfet)
-	return component
+	# Add substrate tap
+	if with_substrate_tap:
+		substrate_tap = tapring(pdk, enclosed_rectangle=pdk.snap_to_2xgrid(evaluate_bbox(top_level.flatten(),padding=pdk.util_max_metal_seperation())))
+		substrate_tap_ref = top_level << movey(substrate_tap,destination=pdk.snap_to_2xgrid(top_level.flatten().center[1],snap4=True))
+		top_level.add_ports(substrate_tap_ref.get_ports_list(),prefix="substratetap_")
+
+	top_level = component_snap_to_grid(rename_ports_by_orientation(top_level))
+	top_level.info['netlist'] = transmission_gate_netlist(
+		pdk, 
+  		width=kwargs.get('width', pmos_width), length=kwargs.get('length', pmos_length), multipliers=1, 
+		subckt_only=True
+	)
+	top_level.info['netlist'].generate_netlist()
+	return top_level
 
 
-@cell
+#@cell
 def long_width_tg(
 	pdk: MappedPDK,
-	component_name: str,
-	pmos_width,
-	pmos_length,
-	nmos_width,
-	nmos_length,
+	component_name: str = "tg",
+	fet_min_width: float = 3,
+	pmos_width: float = 12,
+	pmos_length: float = 0.15,
+	nmos_width: float = 12,
+	nmos_length: float = 0.15,
+	with_substrate_tap: bool = True,
 	add_pin: bool = True, # For LVS
 	**kwargs
 ) -> Component:
 	# To calculate the number of fingers for the underlying PMOS/NMOS layout
-	finger_num = math.ceil(pmos_width / comp_dc.tg_fet_width_base)
-	mos_width = comp_dc.tg_fet_width_base
+	finger_num = math.ceil(pmos_width / fet_min_width)
 
 	# To prepare all necessary cells to construct a transmission gate, i.e.
 	# 1) PMOS
 	# 2) NMOS
-	pfet = pmos(pdk=pdk, multipliers=1, fingers=finger_num, interfinger_rmult=3, gate_rmult=1, with_tie=False, with_substrate_tap=False, with_dummy=(False, False), width=mos_width, length=pmos_length)
-	nfet = nmos(pdk=pdk, multipliers=1, fingers=finger_num, interfinger_rmult=3, gate_rmult=1, with_tie=False, with_dnwell=False, with_substrate_tap=False, with_dummy=(False, False), width=mos_width, length=nmos_length)
+	pfet = pmos(pdk=pdk, multipliers=1, fingers=finger_num, interfinger_rmult=1, gate_rmult=1, with_tie=False, with_substrate_tap=False, with_dummy=(True, True), width=fet_min_width, length=pmos_length)
+	nfet = nmos(pdk=pdk, multipliers=1, fingers=finger_num, interfinger_rmult=1, gate_rmult=1, with_tie=False, with_dnwell=False, with_substrate_tap=False, with_dummy=(True, True), width=fet_min_width, length=nmos_length)
 	top_level = Component(name=component_name)
 	pfet_ref = prec_ref_center(pfet)
 	nfet_ref = prec_ref_center(nfet)
 	top_level.add(pfet_ref)
 	top_level.add(nfet_ref)
 
-	# Placement.step_1:
-	#    a) To vertically flip the NMOS such that the its gate point toward the PMOS's gate
-	#    b) To move the PMOS above the NMOS
+	# Placement (1)
+	#  a) To vertically flip the NMOS such that the its gate point toward the PMOS's gate
+	#  b) To move the PMOS above the NMOS
 	mos_spacing = pdk.util_max_metal_seperation()
 	rename_ports_by_orientation(nfet_ref.mirror_y()) # 
 	pfet_ref.movey(evaluate_bbox(nfet)[1] + mos_spacing)
 
-	# Placement.step_2: instantiation of the stacked vias to the source of every PMOS's finger
+	# Placement (2):
+	#  a) Add a bunch of met1-to-met2 vias attached to the mcron-layered points inside the source of PMOS/NMOS
 	fet_source_finger_num = math.ceil((finger_num+1) / 2)
 	fet_drain_finger_num = finger_num+1-fet_source_finger_num
 	fet_source_fingerL_num = (fet_source_finger_num-1)/2 # excluding the centered one
 	fet_drain_fingerL_num = fet_drain_finger_num/2
 	finger_col_num = fet_source_finger_num # A half of the fingers belongs to drain
-	finger_row_num = math.floor((mos_width*0.5) / (2*pdk.get_grule("met2")["min_width"]))-1 # To pave the stacked vias over all bottom half
-	print(f"finger_row_num: {finger_row_num}, finger_col_num: {finger_col_num}")
+	finger_row_num = math.floor((fet_min_width*0.5) / (2*pdk.get_grule("met2")["min_width"]))-1 # To pave the stacked vias over all bottom half
+#	print(f"finger_row_num: {finger_row_num}, finger_col_num: {finger_col_num}")
 	pmos_source_sdvias = list()
 	nmos_source_sdvias = list()
 	pmos_source_sdvias_ref = list()
@@ -220,8 +295,8 @@ def long_width_tg(
 		top_level.add(pmos_source_sdvias_ref[row][col] for col in range(finger_col_num))
 		top_level.add(nmos_source_sdvias_ref[row][col] for col in range(finger_col_num))
 
-	# Placement.step_3: moving the all the stacked vias associated with the PMOS's source such that
-	#                   they are connected to the PMOS's source
+	# Placement (3): 
+	#  a) Move all the a part of particular stacked vias such that they are connected to the source of PMOS/NMOS
 	centroid_index = math.floor(fet_source_finger_num/2)
 	y_offset = pdk.get_grule("met3")["min_separation"]+pdk.get_grule("met3")["min_width"]
 	x_s2d_offset = abs(pfet_ref.ports["multiplier_0_leftsd_array_row0_col0_bottom_via_S"].center[0]-pfet_ref.ports["multiplier_0_row0_col0_rightsd_array_row0_col0_bottom_via_S"].center[0])
@@ -261,8 +336,8 @@ def long_width_tg(
 	top_level.add_ports(pfet_ref.get_ports_list(), prefix="pmos_")
 	top_level.add_ports(nfet_ref.get_ports_list(), prefix="nmos_")
 
+	# Add pins w/ labels for LVS
 	if add_pin == True:
-		# Add pins w/ labels for LVS
 		top_level.unlock()
 		pin_info = list() # list that contains all port and component information
 		met1_pin=(pdk.get_glayer("met1")[0], 20)
@@ -279,7 +354,7 @@ def long_width_tg(
 		# --- Port: C, i.e. gate control to the NMOS
 		C_pin=rectangle(layer=met1_pin, size=port_size, centered=True).copy()
 		C_pin.add_label(text="C", layer=met1_label)
-		pin_info.append((C_pin, top_level.ports.get(f"nmos_gate_N"), None))
+		pin_info.append((C_pin, top_level.ports.get(f"nmos_gate_S"), None))
 		# --- Port: CBAR, i.e. gate control to the PMOS
 		CBAR_pin=rectangle(layer=met1_pin, size=port_size, centered=True).copy()
 		CBAR_pin.add_label(text="CBAR", layer=met1_label)
@@ -290,50 +365,63 @@ def long_width_tg(
 			alignment = ('c', 'b') if alignment is None else alignment
 			comp_ref = align_comp_to_port(comp, prt, alignment=alignment)
 			top_level.add(comp_ref)
-	
-	component = component_snap_to_grid(rename_ports_by_orientation(top_level))
+			
+	# Add substrate tap
+	if with_substrate_tap:
+		substrate_tap = tapring(pdk, enclosed_rectangle=pdk.snap_to_2xgrid(evaluate_bbox(top_level.flatten(),padding=pdk.util_max_metal_seperation())))
+		substrate_tap_ref = top_level << movey(substrate_tap,destination=pdk.snap_to_2xgrid(top_level.flatten().center[1],snap4=True))
+		top_level.add_ports(substrate_tap_ref.get_ports_list(),prefix="substratetap_")
 
-	component.info['netlist'] = transmission_gate_netlist(
+	top_level = component_snap_to_grid(rename_ports_by_orientation(top_level))
+	top_level.info['netlist'] = transmission_gate_netlist(
 		pdk, 
-  		width=kwargs.get('width', pmos_width), length=kwargs.get('length', pmos_length), multipliers=1, 
+  		width=kwargs.get('width', pmos_width), length=kwargs.get('length', pmos_length), multipliers=finger_num, 
 		subckt_only=True
 	)
-	#component.info['netlist'] = tg_netlist(pmos=pfet, nmos=nfet)
-	return component
+	top_level.info['netlist'].generate_netlist()
 
-def reconfig_tg(
+	return top_level
+
+@cell
+def tg_cell(
 	pdk: MappedPDK,
-	component_name,
-	pmos_width,
-	pmos_length,
-	nmos_width,
-	nmos_length,
+	component_name: str = "tg",
+	fet_min_width: float = 3,
+	pmos_width: float = 12,
+	pmos_length: float = 0.15,
+	nmos_width: float = 12,
+	nmos_length: float = 0.15,
 	add_pin: bool = True, # For LVS
+	with_substrate_tap: bool = True,
 	**kwargs
 ) -> Component:
 	if pmos_width != nmos_width:
 		raise ValueError("PCell constraint: the widths of PMOS and NMOS must be identical")
-	elif (pmos_width % comp_dc.tg_fet_width_factor) != 0:
-		raise ValueError(f"PCell constraint: the widths of PMOS and NMOS must be multiple of {comp_dc.tg_fet_width_factor} (the given width: {pmos_width})")
-	elif pmos_width >= comp_dc.tg_fet_width_base: # Long-width PMOS and NMOS
-		tg = long_width_tg(
-			pdk=pdk,
-			component_name=component_name,
-			pmos_width=pmos_width,
-			pmos_length=pmos_length,
-			nmos_width=nmos_width,
-			nmos_length=nmos_length,
-			add_pin=True
-		)
+	elif pmos_width > fet_min_width: # Long-width PMOS and NMOS
+		if (pmos_width % (fet_min_width*4)) != 0:
+			raise ValueError(f"PCell constraint: the widths of PMOS and NMOS must be a multiple of {fet_min_width*4} um, however the given width is {pmos_width} um")
+		else:
+			tg = long_width_tg(
+				pdk=pdk,
+				component_name=component_name,
+				fet_min_width=fet_min_width,
+				pmos_width=pmos_width,
+				pmos_length=pmos_length,
+				nmos_width=nmos_width,
+				nmos_length=nmos_length,
+				with_substrate_tap=True,
+				add_pin=True
+			)
 	else: # Short-width PMOS and NMOS
 		tg = short_width_tg(
 			pdk=pdk,
 			component_name=component_name,
-			orientation_config={"degree": 270},
+			fet_min_width=fet_min_width,
 			pmos_width=pmos_width,
 			pmos_length=pmos_length,
 			nmos_width=nmos_width,
 			nmos_length=nmos_length,
+			with_substrate_tap=True,
 			add_pin=True
 		)
 
--- openfasoc/generators/glayout/glayout/flow/blocks/transmission_gate_saltychip/comp_dc.py
+++
@@ -1,5 +0,0 @@
-def initialise(): 
-    global inv_fet_width_base, tg_fet_width_base, tg_fet_width_factor
-    inv_fet_width_base = 3.0
-    tg_fet_width_base = 3.0
-    tg_fet_width_factor = tg_fet_width_base*4
