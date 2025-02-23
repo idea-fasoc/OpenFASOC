11340c3eded2568fceeef667313ed40442ea7cc5
Saptarshi Ghosh <sapta15@gmail.com>
2025-02-23 12:58:50 +0100

    helper file added for delete_directory function

--- openfasoc/generators/glayout/glayout/flow/blocks/composite/regulated_cascoded_current_mirror/regulated_cascoded_current_mirror.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/composite/regulated_cascoded_current_mirror/regulated_cascoded_current_mirror.py
@@ -27,20 +27,6 @@ from gdsfactory.routing.route_quad import route_quad
 from glayout.flow.spice.netlist import Netlist
 from typing import Optional, Union 
 
-def delete_files_in_directory(directory_path):
-   try:
-     files = listdir(directory_path)
-     for file in files:
-       file_path = path.join(directory_path, file)
-       if path.isfile(file_path):
-         try:
-            chmod(file_path, 0o777)
-            remove(file_path)
-         except PermissionError:
-               print(f"PermissionError: Operation not permitted for {file_path}")
-     print("All files deleted successfully.")
-   except OSError:
-     print("Error occurred while deleting files.")
 
 global PDK_ROOT
 if 'PDK_ROOT' in environ:
