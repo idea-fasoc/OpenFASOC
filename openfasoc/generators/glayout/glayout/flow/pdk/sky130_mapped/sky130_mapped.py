9083c58d20d19bf8fb4d4834a633a6a05e9ffead
Saptarshi Ghosh <sapta15@gmail.com>
2025-03-27 12:30:02 +0100

    Commenting the existing layer defination style

--- openfasoc/generators/glayout/glayout/flow/pdk/sky130_mapped/sky130_mapped.py
+++ openfasoc/generators/glayout/glayout/flow/pdk/sky130_mapped/sky130_mapped.py
@@ -7,28 +7,28 @@ from ..sky130_mapped.grules import grulesobj
 from pathlib import Path
 from ..sky130_mapped.sky130_add_npc import sky130_add_npc
 
-# use mimcap over metal 3
-sky130_glayer_mapping = {
-    "capmet": (89, 44),
-    "met5": (71,20),
-    "via4": (70,44),
-    "met4": (70,20),
-    "via3": (69,44),
-    "met3": (69,20),
-    "via2": (68,44),
-    "met2": (68,20),
-    "via1": (67,44),
-    "met1": (67,20),
-    "mcon": (66,44),
-    "poly": (66,20),
-    "active_diff": (65,20),
-    "active_tap": (65,20),
-    "n+s/d": (93,44),
-    "p+s/d": (94,20),
-    "nwell": (64,20),
-    "pwell": (64,44),
-    "dnwell": (64,18),
-}
+# # use mimcap over metal 3
+# sky130_glayer_mapping = {
+#     "capmet": (89, 44),
+#     "met5": (71,20),
+#     "via4": (70,44),
+#     "met4": (70,20),
+#     "via3": (69,44),
+#     "met3": (69,20),
+#     "via2": (68,44),
+#     "met2": (68,20),
+#     "via1": (67,44),
+#     "met1": (67,20),
+#     "mcon": (66,44),
+#     "poly": (66,20),
+#     "active_diff": (65,20),
+#     "active_tap": (65,20),
+#     "n+s/d": (93,44),
+#     "p+s/d": (94,20),
+#     "nwell": (64,20),
+#     "pwell": (64,44),
+#     "dnwell": (64,18),
+# }
 
 openfasoc_dir = Path(__file__).resolve().parent.parent.parent.parent.parent.parent.parent
 
