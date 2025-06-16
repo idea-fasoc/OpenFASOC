31e72d9bb373073274db7da5885a5b008b6514e4
Saptarshi Ghosh <sapta15@gmail.com>
2025-06-16 18:24:11 +0200

    successfull run in the IIC docker modified pdk_root paths

--- openfasoc/generators/glayout/glayout/flow/pdk/gf180_mapped/gf180_mapped.py
+++ openfasoc/generators/glayout/glayout/flow/pdk/gf180_mapped/gf180_mapped.py
@@ -5,6 +5,7 @@ usage: from gf180_mapped import gf180_mapped_pdk
 from ..gf180_mapped.grules import grulesobj
 from ..mappedpdk import MappedPDK, SetupPDKFiles
 from pathlib import Path
+import os
 
 # Actual Pin definations for GlobalFoundries 180nmMCU from the PDK manual
 # Ref: https://gf180mcu-pdk.readthedocs.io/en/latest/
@@ -82,7 +83,8 @@ gf180_glayer_mapping = {
 gf180_lydrc_file_path = Path(__file__).resolve().parent / "gf180mcu_drc.lydrc"
 
 openfasoc_dir = Path(__file__).resolve().parent.parent.parent.parent.parent.parent.parent
-pdk_root = Path('/usr/bin/miniconda3/share/pdk/')
+#pdk_root = Path('/usr/bin/miniconda3/share/pdk/')
+pdk_root = Path(os.getenv('PDK_ROOT'))
 lvs_schematic_ref_file = openfasoc_dir / "common" / "platforms" / "gf180osu9t" / "cdl" / "gf180mcu_osu_sc_9T.spice"
 magic_drc_file = pdk_root / "gf180mcuC" / "libs.tech" / "magic" / "gf180mcuC.magicrc"
 lvs_setup_tcl_file = pdk_root / "gf180mcuC" / "libs.tech" / "netgen" / "gf180mcuC_setup.tcl"
