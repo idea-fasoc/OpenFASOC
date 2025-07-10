510665902e79f03858a36bd3e6852d978b03a275
Subham Pal <122683549+Subhampal9@users.noreply.github.com>
2025-07-10 22:15:16 +0530

    added labels for opamp(both two and three stages)

--- openfasoc/generators/glayout/glayout/flow/blocks/elementary/LHS/opamp.py
+++ openfasoc/generators/glayout/glayout/flow/blocks/elementary/LHS/opamp.py
@@ -14,8 +14,57 @@ from glayout.flow.pdk.sky130_mapped import sky130_mapped_pdk as pdk
 from glayout.flow.pdk.util.snap_to_grid import component_snap_to_grid
 from glayout.flow.pdk.util.component_array_create import write_component_matrix
 from evaluator_wrapper import run_evaluation
+def sky130_add_opamp_2_labels(opamp_in: Component) -> Component:
+	"""adds opamp labels for extraction, without adding pads
+	this function does not need to be used with sky130_add_opamp_pads
+	"""
+	opamp_in.unlock()
+	# define layers
+	met2_pin = (69,16)
+	met2_label = (69,5)
+	met3_pin = (70,16)
+	met3_label = (70,5)
+	met4_pin = (71,16)
+	met4_label = (71,5)
+	# list that will contain all port/comp info
+	move_info = list()
+	# create labels and append to info list
+	# gnd
+	gndlabel = rectangle(layer=met3_pin,size=(1,1),centered=True).copy()
+	gndlabel.add_label(text="GND",layer=met3_label)
+	move_info.append((gndlabel,opamp_in.ports["pin_gnd_N"],None))
+	#diffpairibias
+	ibias1label = rectangle(layer=met2_pin,size=(1,1),centered=True).copy()
+	ibias1label.add_label(text="DIFFPAIR_BIAS",layer=met2_label)
+	move_info.append((ibias1label,opamp_in.ports["pin_diffpairibias_N"],None))
+	# commonsourceibias
+	ibias2label = rectangle(layer=met4_pin,size=(1,1),centered=True).copy()
+	ibias2label.add_label(text="CS_BIAS",layer=met4_label)
+	move_info.append((ibias2label,opamp_in.ports["pin_commonsourceibias_N"],None))
+	#minus
+	minuslabel = rectangle(layer=met2_pin,size=(1,1),centered=True).copy()
+	minuslabel.add_label(text="VP",layer=met2_label)
+	move_info.append((minuslabel,opamp_in.ports["pin_minus_N"],None))
+	#-plus
+	pluslabel = rectangle(layer=met2_pin,size=(1,1),centered=True).copy()
+	pluslabel.add_label(text="VN",layer=met2_label)
+	move_info.append((pluslabel,opamp_in.ports["pin_plus_N"],None))
+	#vdd
+	vddlabel = rectangle(layer=met3_pin,size=(1,1),centered=True).copy()
+	vddlabel.add_label(text="VDD",layer=met3_label)
+	move_info.append((vddlabel,opamp_in.ports["pin_vdd_N"],None))
+	# output (2nd stage)
+	outputlabel = rectangle(layer=met4_pin,size=(0.2,0.2),centered=True).copy()
+	outputlabel.add_label(text="VOUT",layer=met4_label)
+	move_info.append((outputlabel,opamp_in.ports["commonsource_output_E"],('l','c')))
+	# move everything to position
+	for comp, prt, alignment in move_info:
+		alignment = ('c','b') if alignment is None else alignment
+		compref = align_comp_to_port(comp, prt, alignment=alignment)
+		opamp_in.add(compref)
+	return opamp_in.flatten()
 
-def sky130_add_opamp_labels(opamp_in: Component) -> Component:
+def sky130_add_opamp_3_labels(opamp_in: Component) -> Component:
 	"""adds opamp labels for extraction, without adding pads
 	this function does not need to be used with sky130_add_opamp_pads
 	"""
@@ -74,8 +123,8 @@ def sky130_add_opamp_labels(opamp_in: Component) -> Component:
 	return opamp_in.flatten()
 
 if __name__=="__main__":
-    opamp_comp = sky130_add_opamp_labels(opamp(pdk, add_output_stage=False))
-    opamp_comp.show()
+    opamp_comp = sky130_add_opamp_2_labels(opamp(pdk, add_output_stage=False))
+    #opamp_comp.show()
     opamp_comp.name = "opamp"
     #magic_drc_result = pdk.drc_magic(opamp_comp, opamp_comp.name)
     #netgen_lvs_result = pdk.lvs_netgen(opamp_comp, opamp_comp.name)
